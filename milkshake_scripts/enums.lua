66ffc7dbb23261fb20362285581878c27aad70d0
eucalyptusJ <88509866+eucalyptusJ@users.noreply.github.com>
2023-01-19 15:49:04 -0500

    Update 3 files, create 1 file
    -Create lachancla.lua
    -Implement La Chancla

--- milkshake_scripts/enums.lua
+++ milkshake_scripts/enums.lua
@@ -7,7 +7,8 @@ enums.Collectibles = {
     DICE_DICE = Isaac.GetItemIdByName("Dice Dice"),
     MILKSHAKE = Isaac.GetItemIdByName("Milkshake!"),
     FIRECRACKER_ROSE = Isaac.GetItemIdByName("Firecracker Rose"),
-    SHARP_CURSOR = Isaac.GetItemIdByName("Sharp Cursor")
+    SHARP_CURSOR = Isaac.GetItemIdByName("Sharp Cursor"),
+    LA_CHANCLA = Isaac.GetItemIdByName("La Chancla")
 }
 
 enums.Trinkets = {
