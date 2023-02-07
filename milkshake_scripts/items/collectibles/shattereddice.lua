a1d61f0e26e4ec2c8217af2818033960d5124e97
eucalyptusJ <88509866+eucalyptusJ@users.noreply.github.com>
2023-02-07 17:40:11 -0500

    Update shattereddice.lua
    -Change how the item interacts with choice pedestals
    -Quality 0 items now give random pickups
    -Items now spawn directly left and right of the affected item
    -Items spawned now take from the last item pool that generated an item instead of being random

--- milkshake_scripts/items/collectibles/shattereddice.lua
+++ milkshake_scripts/items/collectibles/shattereddice.lua
@@ -1,9 +1,24 @@
 local shatteredDice = {}
 local enums = require("milkshake_scripts.enums")
 
-local STEP = 40
+local SHIFT_RIGHT = Vector(40, 0)
+local SHIFT_LEFT = Vector(-40, 0)
+local PICKUPS_TO_SPAWN = 6
+
+local function getCollectibleCount()
+    local collectibleCount = 0
+    for i, entity in pairs(Isaac.GetRoomEntities()) do
+        if entity.Type == EntityType.ENTITY_PICKUP
+        and entity.Variant == PickupVariant.PICKUP_COLLECTIBLE then
+            collectibleCount = collectibleCount + 1
+        end
+    end
+    return collectibleCount
+end
+
 
 function shatteredDice:onUse(collectible, rng, player)
+    local collectibleCount = getCollectibleCount()
     for i, entity in pairs(Isaac.GetRoomEntities()) do
         if entity.Type == EntityType.ENTITY_PICKUP
         and entity.Variant == PickupVariant.PICKUP_COLLECTIBLE then
@@ -11,15 +26,35 @@ function shatteredDice:onUse(collectible, rng, player)
             local collectibleQuality =  Isaac.GetItemConfig():GetCollectible(collectible.SubType).Quality
             collectible:Remove()
 
-            local newCollectible
-            for i = 0, 1 do
-                repeat
-                    newCollectible = Game():GetItemPool():GetCollectible(rng:RandomInt(31))
-                until Isaac.GetItemConfig():GetCollectible(newCollectible).Quality == collectibleQuality - 1
+            local newCollectibleID
+            if collectibleQuality - 1 >= 0 then
+                for i = 0, 1 do
+                    
+                    repeat
+                        local itemPool = Game():GetItemPool()
+                        newCollectibleID = itemPool:GetCollectible(itemPool:GetLastPool())
+                    until Isaac.GetItemConfig():GetCollectible(newCollectibleID).Quality == collectibleQuality - 1
 
-                local spawnPosition = Isaac.GetFreeNearPosition(collectible.Position, STEP)
-                Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, newCollectible, spawnPosition, Vector(0,0), nil)
-                local collectible = newCollectible
+                    local spawnPosition
+
+                    if i == 0 then
+                        spawnPosition = collectible.Position + SHIFT_LEFT
+                    elseif i == 1 then
+                        spawnPosition = collectible.Position + SHIFT_RIGHT
+                    end
+        
+                    local shatteredCollectible = Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, newCollectibleID, spawnPosition, Vector(0,0), nil):ToPickup()
+                    
+                    if i == 0 then
+                        shatteredCollectible.OptionsPickupIndex = collectible.OptionsPickupIndex
+                    elseif i == 1 then
+                        shatteredCollectible.OptionsPickupIndex = collectible.OptionsPickupIndex + 1
+                    end
+                end
+            else
+                for i = 1, PICKUPS_TO_SPAWN do
+                    Isaac.Spawn(EntityType.ENTITY_PICKUP, 0, 0, collectible.Position, RandomVector(), player)
+                end
             end
         end
     end
