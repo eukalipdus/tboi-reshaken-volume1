5c4ff3c5aa9d1ea362a1aa2fc0ff66c234544f48
cristobalmartinarenas <100757545+cristobalmartinarenas@users.noreply.github.com>
2023-02-09 19:08:41 +0100

    chore: Added spanish translations

--- milkshake_scripts/modcompatibility/descriptions.lua
+++ milkshake_scripts/modcompatibility/descriptions.lua
@@ -56,44 +56,72 @@ descriptions.Collectibles = {
     },
     [enums.Collectibles.SHARP_CURSOR] = {
         en_us = {
-            name = "Sharp Cursors",
-            description = "#Spawns a familiar that targets the furthest enemy from the player #Pressing a shooting key makes it deal 10% player's damage. #Works with Mouse Control!",
+            name = "Sharp Cursor",
+            description = "#Spawns a familiar that targets the furthest enemy from the player #Pressing a shooting key makes it deal 10% player's damage #Works with Mouse Control!",
+        },
+        spa = {
+            name = "Cursor Afilado",
+            description = "#Genera un familiar que va hacia el enemigo mas alejado #Pulsar cualquier tecla de disparo hace que haga un 10% del daño del jugador #Funciona con los Controles de Raton!"
         },
     },
     [enums.Collectibles.BLACK_EYE] = {
         en_us = {
             name = "Black Eye",
-            description = "{{Blank}}{{ArrowUp}} +0.7 tears up and knockback up for the right eye only. #Currently unused and uncoded, maybe it will show up later (It probably wont).",
+            description = "{{Blank}}{{ArrowUp}} +0.7 tears up and knockback up for the right eye only #Currently unused and uncoded, maybe it will show up later (It probably wont)",
+        },
+        spa = {
+            name = "Ojo Morado",
+            description = "{{Blank}}{{ArrowUp}} +0.7 lagrimas y empuje solo para el ojo derecho #De momento sin usar y sin programar, puede que aparezca despues (Probablemente no)"
         },
     },
     [enums.Collectibles.DICE_DICE] = {
         en_us = {
             name = "Dice Dice",
-            description = "Activates a random dice room effect. #Currently unused and uncoded, maybe it will show up later.",
+            description = "Activates a random dice room effect #Currently unused and uncoded, maybe it will show up later",
+        },
+        spa = {
+            name = "Dado Dado",
+            description = "Activa un efecto de la habitacion de dado aleatorio #De momento sin usar y sin programar, puede que aparezca despues"
         },
     },
     [enums.Collectibles.FIRE_CRACKER_ROSE] = {
         en_us = {
             name = "Firecracker Rose",
-            description = "{{Burning}} Chance to shoot a seed tear that inflicts enemies with Kabloom. #Kabloomed enemies will explode into exploding petal tears after 5 seconds.",
+            description = "{{Burning}} Chance to shoot a seed tear that inflicts enemies with Kabloom #Kabloomed enemies will explode into exploding petal tears after 5 seconds",
+        },
+        spa = {
+            name = "Rosa De Fuego",
+            description = "{{Burning}} Posibilidad de disparar una semilla que inflinge a los enemigos con Kabloom. #Los enemigos con Kabloom explotan en petalos despues de 5 segundos"
         },
     },
     [enums.Collectibles.GLOBIN_IN_A_BUCKET] = {
         en_us = {
             name = "Globin In A Bucket",
-            description = "Spawns a friendly globin that fights by your side.",
+            description = "Spawns a friendly globin that fights by your side",
+        },
+        spa = {
+            name = "Globin En Un Cubo",
+            description = "Genera un globin amistoso que lucha por ti"
         },
     },
     [enums.Collectibles.GOLDEN_SHOVEL] = {
         en_us = {
             name = "Golden Shovel",
-            description = "On use, digs into the ground, spawning 1-2 golden chests and a random golden pickup, as well as digging up a golden crawlspace if used on decoration tiles.",
+            description = "On use, digs into the ground, spawning 1-2 golden chests and a random golden pickup, as well as digging up a golden crawlspace if used on decoration tiles",
+        },
+        spa = {
+            name = "Pala Dorada",
+            description = "Cava en el suelo, generando 1-2 cofres dorados y un pickup dorado aleatorio. Ademas, cavar en una decoracion genera un acceso a la tienda secreta"
         },
     },
     [enums.Collectibles.LA_CHANCLA] = {
         en_us = {
             name = "La Chancla",
-            description = "{{Blank}} {{Speed}} 0.3 Speed up. #Makes Isaac immune to stomping attacks (Such as {{MomBossSmall}} Mom and {{SatanSmall}} Satan).",
+            description = "{{Blank}} {{Speed}} 0.3 Speed up #Makes Isaac immune to stomping attacks (Such as {{MomBossSmall}} Mom and {{SatanSmall}} Satan)",
+        },
+        spa = {
+            name = "La Chancla",
+            description = "{{Blank}} {{Speed}} 0.3 de velocidad #Hace a Isaac inmune a los ataques de pisotones (Como {{MomBossSmall}} Mama o {{SatanSmall}} Satan)"
         },
     },
 }
@@ -105,6 +133,10 @@ descriptions.Trinkets = {
             name = "Tungsten Cube",
             description = "{{Blank}}{{ArrowDown}} Grants a -0.2 speed down #Dropping it creates a huge shockwaves that deals big damage",
         },
+        spa = {
+            name = "Cubo De Tungsteno",
+            description = "{{Blank}}{{ArrowDown}} Otorga -0.2 de velocidad #Soltarlo crea una gran onda de choque que hace un gran daño",
+        },
     },
 }
 
@@ -115,6 +147,10 @@ descriptions.Cards = {
             name = "Druidity Orb",
             description = "#Wraps all enemies in the room in vines for 20 seconds, freezing them #Killing a vined enemy spawns a fruit heart"
         },
+        spa = {
+            name = "Orbe Druidico",
+            description = "#Enreda a todos los enemigos en enredaderas durante 20 segundos, parandolos #Matar a un enemigo enredado genera un corazon frutal"
+        }
     }
 }
 
