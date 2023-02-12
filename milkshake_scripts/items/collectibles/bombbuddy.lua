b7cf62c49ac0764a857624e10ec5ede2dd5efe64
Hannah Hadfield <robotooth13alternative@gmail.com>
2023-02-12 00:38:51 +0000

    WTF Breadmund why are they called Bomb Bums and not Bomb Beggars

--- milkshake_scripts/items/collectibles/bombbuddy.lua
+++ milkshake_scripts/items/collectibles/bombbuddy.lua
@@ -2,10 +2,12 @@ local bombBuddy = {}
 local enums = require("milkshake_scripts.enums")
 
 function bombBuddy:EvaluateCache(player)
+	print(enums.Collectibles.BOMB_BUM)
+	print(enums.Familiars.BOMB_BUM)
     TSIL.Familiars.CheckFamiliarFromCollectibles(
         player,
-        enums.Collectibles.BOMB_BUDDY,
-        enums.Familiars.BOMB_BUDDY
+        enums.Collectibles.BOMB_BUM,
+        enums.Familiars.BOMB_BUM
     )
 end
 milkshakeMod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, bombBuddy.EvaluateCache, CacheFlag.CACHE_FAMILIARS)
@@ -14,10 +16,10 @@ milkshakeMod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, bombBuddy.EvaluateCache
 function bombBuddy:FamiliarInit(familiar)
 	--IDEK if i need this
 end
-milkshakeMod:AddCallback(ModCallbacks.MC_FAMILIAR_INIT, bombBuddy.FamiliarInit, enums.Familiars.BOMB_BUDDY)
+milkshakeMod:AddCallback(ModCallbacks.MC_FAMILIAR_INIT, bombBuddy.FamiliarInit, enums.Familiars.BOMB_BUM)
 
 ---@param familiar EntityFamiliar
 function bombBuddy:FamiliarUpdate(familiar)
 	familiar:FollowParent()
 end
-milkshakeMod:AddCallback(ModCallbacks.MC_FAMILIAR_UPDATE, bombBuddy.FamiliarUpdate, enums.Familiars.BOMB_BUDDY)
+milkshakeMod:AddCallback(ModCallbacks.MC_FAMILIAR_UPDATE, bombBuddy.FamiliarUpdate, enums.Familiars.BOMB_BUM)
