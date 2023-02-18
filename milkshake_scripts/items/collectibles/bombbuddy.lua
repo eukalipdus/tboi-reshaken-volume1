d16ee8ee45de32c79b27470dc90bf5b9bb2a9431
Hannah Hadfield <robotooth13alternative@gmail.com>
2023-02-18 22:59:19 +0000

    Added literally 2 characters to bomb buddy so the debugger will shut up (still won't work probably)

--- milkshake_scripts/items/collectibles/bombbuddy.lua
+++ milkshake_scripts/items/collectibles/bombbuddy.lua
@@ -1,4 +1,4 @@
 local enums = require("milkshake_scripts.enums")
 local bumAPI = require("milkshake_scripts.bumAPI")
 
-bumAPI:AddBumFamiliar(enums.Familiars.BOMB_BUM, enums.Collectibles.BOMB_BUM, true, {}, {})
+bumAPI:AddBumFamiliar(enums.Familiars.BOMB_BUM, enums.Collectibles.BOMB_BUM, true, 3, {}, {})
