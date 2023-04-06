6ff310c59c839770f9535f2b59973f3325a6a89e
thicco-catto <cristobal.martin01@gmail.com>
2023-04-06 17:26:32 +0200

    feat: Added leviticus compat for most of ff stuff

--- milkshake_scripts/items/collectibles/active/leviticus.lua
+++ milkshake_scripts/items/collectibles/active/leviticus.lua
@@ -1,225 +1,328 @@
 local enums = milkshakeMod.enums
-local leviticus = {}
+local Leviticus = {}
 
-local usedLeviticus = 0
+---@class LeviticusSoulHeartInfo
+---@field charges integer
+---@field soundEffect SoundEffect | integer
+---@field extraHeart LeviticusExtraHeartTypes
+---@field isBlended boolean
+
+---@enum LeviticusExtraHeartTypes
+local EXTRA_HEART_TYPES = {
+    SOUL = "soul",
+    BLACK = "black",
+    IMMORAL = "immoral"
+}
+
+TSIL.SaveManager.AddPersistentVariable(
+    milkshakeMod,
+    "UsedLeviticus",
+    false,
+    TSIL.Enums.VariablePersistenceMode.RESET_LEVEL
+)
 
 local LEVITICUS_MAX_CHARGES = Isaac.GetItemConfig():GetCollectible(enums.Collectibles.LEVITICUS).MaxCharges
 
+
+---@type table<HeartSubType, LeviticusSoulHeartInfo>
 local VANILLA_SOUL_HEARTS = {
-    [HeartSubType.HEART_SOUL] = true, 
-    [HeartSubType.HEART_BLACK] = true,
-    [HeartSubType.HEART_BLENDED] = true,
-    [HeartSubType.HEART_HALF_SOUL] = true
-  }
-if FiendFolio then
-local FF_SOUL_HEARTS = {
-    [FiendFolio.PICKUP.VARIANT.HALF_IMMORAL_HEART] = true,
-    [FiendFolio.PICKUP.VARIANT.IMMORAL_HEART] = true,
-    [FiendFolio.PICKUP.VARIANT.BLENDED_IMMORAL_HEART] = true,
-    [FiendFolio.PICKUP.VARIANT.BLENDED_BLACK_HEART] = true,
-    [FiendFolio.PICKUP.VARIANT.HALF_BLACK_HEART] = true,
-    
+    [HeartSubType.HEART_SOUL] = {
+        charges = 2,
+        soundEffect = SoundEffect.SOUND_HOLY,
+        extraHeart = EXTRA_HEART_TYPES.SOUL,
+        isBlended = false
+    },
+    [HeartSubType.HEART_BLACK] = {
+        charges = 2,
+        soundEffect = SoundEffect.SOUND_UNHOLY,
+        extraHeart = EXTRA_HEART_TYPES.BLACK,
+        isBlended = false
+    },
+    [HeartSubType.HEART_BLENDED] = {
+        charges = 2,
+        soundEffect = SoundEffect.SOUND_HOLY,
+        extraHeart = EXTRA_HEART_TYPES.SOUL,
+        isBlended = true
+    },
+    [HeartSubType.HEART_HALF_SOUL] = {
+        charges = 1,
+        soundEffect = SoundEffect.SOUND_HOLY,
+        extraHeart = EXTRA_HEART_TYPES.SOUL,
+        isBlended = false
+    }
 }
+
+---@type table<PickupVariant, LeviticusSoulHeartInfo>
+local FF_SOUL_HEARTS = {}
+if FiendFolio then
+    FF_SOUL_HEARTS = {
+        [FiendFolio.PICKUP.VARIANT.HALF_IMMORAL_HEART] = {
+            charges = 1,
+            soundEffect = FiendFolio.Sounds.FiendHeartPickup,
+            extraHeart = EXTRA_HEART_TYPES.IMMORAL,
+            isBlended = false
+        },
+        [FiendFolio.PICKUP.VARIANT.IMMORAL_HEART] = {
+            charges = 2,
+            soundEffect = FiendFolio.Sounds.FiendHeartPickup,
+            extraHeart = EXTRA_HEART_TYPES.IMMORAL,
+            isBlended = false
+        },
+        [FiendFolio.PICKUP.VARIANT.BLENDED_IMMORAL_HEART] = {
+            charges = 2,
+            soundEffect = FiendFolio.Sounds.FiendHeartPickup,
+            extraHeart = EXTRA_HEART_TYPES.IMMORAL,
+            isBlended = true
+        },
+        [FiendFolio.PICKUP.VARIANT.BLENDED_BLACK_HEART] = {
+            charges = 2,
+            soundEffect = SoundEffect.SOUND_UNHOLY,
+            extraHeart = EXTRA_HEART_TYPES.BLACK,
+            isBlended = true
+        },
+        [FiendFolio.PICKUP.VARIANT.HALF_BLACK_HEART] = {
+            charges = 1,
+            soundEffect = SoundEffect.SOUND_UNHOLY,
+            extraHeart = EXTRA_HEART_TYPES.BLACK,
+            isBlended = true
+        },
+    }
 end
 
-function leviticus:onLeviticusUse(item, _, player)
+function Leviticus:onLeviticusUse(_, _, player)
     player:AddEternalHearts(1)
     SFXManager():Play(SoundEffect.SOUND_SUPERHOLY)
-    usedLeviticus = 1
+
+    TSIL.SaveManager.SetPersistentVariable(
+        milkshakeMod,
+        "UsedLeviticus",
+        true
+    )
+
     return {
         Discharge = true,
         Remove = false,
         ShowAnim = true
     }
 end
---heartTypes: 2 = HEART_SOUL, 4 = HEART_BLACK, 10 = PICKUP_IMMORAL_HEART
-function leviticus:addSoulCharges(player, charges, heartType)
-    for i = 0, charges-1, 1 do
-        local leviticus_slot = leviticus:checkActiveSlot(player)
 
-        if player:GetActiveCharge(leviticus_slot) < LEVITICUS_MAX_CHARGES then
-        player:SetActiveCharge(player:GetActiveCharge(leviticus_slot) + 1, leviticus_slot)
+milkshakeMod:AddCallback(
+    ModCallbacks.MC_USE_ITEM,
+    Leviticus.onLeviticusUse,
+    enums.Collectibles.LEVITICUS
+)
+
+
+local function CheckLeviticusActiveSlot(player)
+    for i = 0, 4, 1 do
+        if player:GetActiveItem(i) == enums.Collectibles.LEVITICUS and
+        player:GetActiveCharge(i) < LEVITICUS_MAX_CHARGES then
+            return i
+        end
+    end
+    return nil
+end
+
+
+---@param player EntityPlayer
+---@param soulHeartInfo LeviticusSoulHeartInfo
+local function AddSoulHeartCharges(player, soulHeartInfo)
+    local slot = CheckLeviticusActiveSlot(player)
+
+    if not slot then return end
+
+    if soulHeartInfo.soundEffect > 0 then
+        SFXManager():Play(soulHeartInfo.soundEffect)
+    end
+
+    for _ = 0, soulHeartInfo.charges - 1, 1 do
+        if player:GetActiveCharge(slot) < LEVITICUS_MAX_CHARGES then
+            player:SetActiveCharge(player:GetActiveCharge(slot) + 1, slot)
         else
-            if heartType == 2 then player:AddSoulHearts(1) end
-            if heartType == 4 then player:AddBlackHearts(1) end
-            if heartType == 10 then FiendFolio:AddImmoralHearts(player, 1) end
+            if soulHeartInfo.extraHeart == EXTRA_HEART_TYPES.SOUL then
+                player:AddSoulHearts(1)
+            elseif soulHeartInfo.extraHeart == EXTRA_HEART_TYPES.BLACK then
+                player:AddBlackHearts(1)
+            elseif soulHeartInfo.extraHeart == EXTRA_HEART_TYPES.IMMORAL then
+                FiendFolio:AddImmoralHearts(player, 1)
+            end
         end
     end
 end
 
-function leviticus:checkActiveSlot(player)
-for i = 0, 4, 1 do
-    if player:GetActiveItem(i) == enums.Collectibles.LEVITICUS and player:GetActiveCharge(i) < LEVITICUS_MAX_CHARGES then
-        return i
-    end
-end
-return nil
-end
 
-function leviticus:removePickup(pickup)
+local function RemovePickup(pickup)
     pickup = pickup:ToPickup()
-    
+
     local sprite = pickup:GetSprite()
     sprite:RemoveOverlay()
     sprite:Play("Collect", true)
     pickup:Die()
 end
 
-function leviticus:onPickupCollision(pickup, collider)
+
+---@param pickup EntityPickup
+---@param collider Entity
+function Leviticus:onPickupCollision(pickup, collider)
     local player = collider:ToPlayer()
     if not player then return end
 
     if not player:HasCollectible(enums.Collectibles.LEVITICUS) then return end
 
-    local leviticus_slot = leviticus:checkActiveSlot(player)
+    --Check if there is any slot with empty charges
+    local leviticus_slot = CheckLeviticusActiveSlot(player)
     if leviticus_slot == nil then return end
 
-    if player:CanPickSoulHearts() and (player:GetPlayerType() ~= PlayerType.PLAYER_THELOST_B and player:GetPlayerType() ~= PlayerType.PLAYER_THELOST) then return end
+    --Can player actually pick it up?
+    -- if player:CanPickSoulHearts() and
+    -- (
+    --     player:GetPlayerType() ~= PlayerType.PLAYER_THELOST_B and
+    --     player:GetPlayerType() ~= PlayerType.PLAYER_THELOST
+    -- ) then return end
 
-    if not (VANILLA_SOUL_HEARTS[pickup.SubType] or (FiendFolio and FF_SOUL_HEARTS[pickup.Variant])) then return end
-        if pickup:IsShopItem() and pickup.Price > player:GetNumCoins() then return 
-        elseif pickup:IsShopItem() and pickup.Price <= player:GetNumCoins() then
+    local soulHeartInfo
+
+    if pickup.Variant == PickupVariant.PICKUP_HEART then
+        soulHeartInfo = VANILLA_SOUL_HEARTS[pickup.SubType]
+    elseif FiendFolio then
+        soulHeartInfo = FF_SOUL_HEARTS[pickup.Variant]
+    end
+
+    if not soulHeartInfo then return end
+
+    if pickup:IsShopItem() then
+        --It's a shop item we can't pay for
+        if pickup.Price > player:GetNumCoins() then
+            return
+        else
             player:AddCoins(-1 * pickup.Price)
         end
-
-    if pickup.SubType == HeartSubType.HEART_SOUL then
-        leviticus:addSoulCharges(player, 2, 2)
-        SFXManager():Play(SoundEffect.SOUND_HOLY)
-        leviticus:removePickup(pickup)
-        return false
-    elseif pickup.SubType == HeartSubType.HEART_HALF_SOUL then
-        leviticus:addSoulCharges(player, 1, 2)
-        SFXManager():Play(SoundEffect.SOUND_HOLY)
-        leviticus:removePickup(pickup)
-        return false
-        elseif pickup.SubType == HeartSubType.HEART_BLENDED then
-            local redHeartsToAdd = math.min(2, player:GetMaxHearts() - player:GetHearts())
-            if redHeartsToAdd >= 2 then return
-            elseif redHeartsToAdd == 1 then player:AddHearts(1)
-                SFXManager():Play(SoundEffect.SOUND_BOSS2_BUBBLES)
-                leviticus:addSoulCharges(player, 1, 2)
-                leviticus:removePickup(pickup)
-                SFXManager():Play(SoundEffect.SOUND_HOLY)
-                return false
-            elseif redHeartsToAdd == 0 then
-                leviticus:addSoulCharges(player, 2, 2)
-                leviticus:removePickup(pickup)
-                SFXManager():Play(SoundEffect.SOUND_HOLY)
-                return false
-            end
-
-        elseif pickup.SubType == HeartSubType.HEART_BLACK then
-            leviticus:addSoulCharges(player, 2, 4)
-            SFXManager():Play(SoundEffect.SOUND_UNHOLY)
-            leviticus:removePickup(pickup)
-            return false
-        
-        elseif (FiendFolio and pickup.Variant == FiendFolio.PICKUP.VARIANT.HALF_BLACK_HEART) then
-            leviticus:addSoulCharges(player, 1, 4)
-            SFXManager():Play(SoundEffect.SOUND_UNHOLY)
-            leviticus:removePickup(pickup)
-            return false
-        elseif (FiendFolio and pickup.Variant == FiendFolio.PICKUP.VARIANT.BLENDED_BLACK_HEART) then
-            local redHeartsToAdd = math.min(2, player:GetMaxHearts() - player:GetHearts())
-            if redHeartsToAdd >= 2 then return
-            elseif redHeartsToAdd == 1 then player:AddHearts(1)
-                SFXManager():Play(SoundEffect.SOUND_BOSS2_BUBBLES)
-                leviticus:addSoulCharges(player, 1, 4)
-                leviticus:removePickup(pickup)
-                SFXManager():Play(SoundEffect.SOUND_UNHOLY)
-                return false
-            elseif redHeartsToAdd == 0 then
-                leviticus:addSoulCharges(player, 2, 4)
-                leviticus:removePickup(pickup)
-                SFXManager():Play(SoundEffect.SOUND_UNHOLY)
-                return false
-            end
-
-        elseif (FiendFolio and pickup.Variant == FiendFolio.PICKUP.VARIANT.IMMORAL_HEART) then
-            leviticus:addSoulCharges(player, 2, 10)
-            SFXManager():Play(FiendFolio.Sounds.FiendHeartPickup, 1, 0, false, 1)
-            leviticus:removePickup(pickup)
-            return false
-        elseif (FiendFolio and pickup.Variant == FiendFolio.PICKUP.VARIANT.HALF_IMMORAL_HEART) then
-            leviticus:addSoulCharges(player, 1, 10)
-            SFXManager():Play(FiendFolio.Sounds.FiendHeartPickup, 1, 0, false, 1)
-            leviticus:removePickup(pickup)
-            return false
-        elseif (FiendFolio and pickup.Variant == FiendFolio.PICKUP.VARIANT.BLENDED_IMMORAL_HEART) then
-            local redHeartsToAdd = math.min(2, player:GetMaxHearts() - player:GetHearts())
-            if redHeartsToAdd >= 2 then return
-            elseif redHeartsToAdd == 1 then player:AddHearts(1)
-                SFXManager():Play(SoundEffect.SOUND_BOSS2_BUBBLES)
-                leviticus:addSoulCharges(player, 1, 10)
-                leviticus:removePickup(pickup)
-                SFXManager():Play(FiendFolio.Sounds.FiendHeartPickup, 1, 0, false, 1)
-                return false
-            elseif redHeartsToAdd == 0 then
-                leviticus:addSoulCharges(player, 2, 10)
-                leviticus:removePickup(pickup)
-                SFXManager():Play(FiendFolio.Sounds.FiendHeartPickup, 1, 0, false, 1)
-                return false
-            end
     end
 
+    if soulHeartInfo.isBlended then
+        --Special case for blended hearts
+        local redHeartsToAdd = math.min(2, player:GetMaxHearts() - player:GetHearts())
+        if redHeartsToAdd >= 2 then
+            return
+        elseif redHeartsToAdd == 1 then
+            player:AddHearts(1)
+            SFXManager():Play(SoundEffect.SOUND_BOSS2_BUBBLES)
+
+            soulHeartInfo = {
+                charges = 1,
+                soundEffect = soulHeartInfo.soundEffect,
+                extraHeart = soulHeartInfo.extraHeart
+            }
+        end
+    end
+
+    AddSoulHeartCharges(player, soulHeartInfo)
+
+    RemovePickup(pickup)
+
+    return false
 end
-function leviticus:onItemSpawn(itemPoolType, decrease, _)
+milkshakeMod:AddCallback(
+    ModCallbacks.MC_PRE_PICKUP_COLLISION,
+    Leviticus.onPickupCollision
+)
 
+
+if FiendFolio then
+    function Leviticus.PreHealthAddCHAPI(player, key, hp)
+        if key == "SOUL_HEART" then
+            AddSoulHeartCharges(player, {
+                charges = hp,
+                extraHeart = EXTRA_HEART_TYPES.SOUL,
+                soundEffect = -1
+            })
+            return true
+        elseif key == "BLACK_HEART" then
+            AddSoulHeartCharges(player, {
+                charges = hp,
+                extraHeart = EXTRA_HEART_TYPES.BLACK,
+                soundEffect = -1
+            })
+            return true
+        elseif key == "IMMORAL_HEART" then
+            AddSoulHeartCharges(player, {
+                charges = hp,
+                extraHeart = EXTRA_HEART_TYPES.IMMORAL,
+                soundEffect = -1
+            })
+            return true
+        end
+    end
+
+    CustomHealthAPI.Library.AddCallback(
+        "Milkshake",
+        CustomHealthAPI.Enums.Callbacks.PRE_ADD_HEALTH,
+        0,
+        Leviticus.PreHealthAddCHAPI
+    )
+else
+    ---@param player EntityPlayer
+    ---@param healthType HealthType
+    ---@param old integer
+    ---@param new integer
+    function Leviticus:OnHealthChanged(player, healthType, old, new)
+        if not player:HasCollectible(enums.Collectibles.LEVITICUS) then return end
+        local leviticus_slot = CheckLeviticusActiveSlot(player)
+        if leviticus_slot == nil then return end
+    
+        local heartsAdded = new - old
+        if heartsAdded < 0 then
+            return
+        end
+    
+        if healthType == TSIL.Enums.HealthType.SOUL then
+            player:AddSoulHearts(-heartsAdded)
+            AddSoulHeartCharges(player, {
+                charges = heartsAdded,
+                extraHeart = EXTRA_HEART_TYPES.SOUL,
+                soundEffect = -1
+            })
+        elseif healthType == TSIL.Enums.HealthType.BLACK then
+            player:AddBlackHearts(-heartsAdded)
+            AddSoulHeartCharges(player, {
+                charges = heartsAdded,
+                extraHeart = EXTRA_HEART_TYPES.BLACK,
+                soundEffect = -1
+            })
+        end
+    end
+    milkshakeMod:AddCallback(
+        TSIL.Enums.CustomCallback.POST_PLAYER_HEALTH_CHANGED,
+        Leviticus.OnHealthChanged
+    )
+end
+
+
+function Leviticus:onItemSpawn(itemPoolType, _, seed)
     local roomType = Game():GetRoom():GetType()
-    if roomType ~= RoomType.ROOM_BOSS then
-        return
-    end
-    if itemPoolType ~= ItemPoolType.POOL_BOSS then
-        return
-    end
-    if usedLeviticus == 0 then
-        return
-    end
+    if roomType ~= RoomType.ROOM_BOSS then return end
+    if itemPoolType ~= ItemPoolType.POOL_BOSS then return end
+
+    local usedLeviticus = TSIL.SaveManager.GetPersistentVariable(
+        milkshakeMod,
+        "UsedLeviticus"
+    )
+
+    if not usedLeviticus then return end
+
+    TSIL.SaveManager.SetPersistentVariable(
+        milkshakeMod,
+        "UsedLeviticus",
+        false
+    )
 
     local ItemPool = Game():GetItemPool()
 
-    local randomAngelItemID = ItemPool:GetCollectible(ItemPoolType.POOL_ANGEL, false)
+    local randomAngelItemID = ItemPool:GetCollectible(ItemPoolType.POOL_ANGEL, true, seed)
 
     return randomAngelItemID
-
 end
-
-function leviticus:OnHealthChanged(player, healthType, old, new)
-
-    if not player:HasCollectible(enums.Collectibles.LEVITICUS) then return end
-    local leviticus_slot = leviticus:checkActiveSlot(player)
-    if leviticus_slot == nil then return end
-    if healthType ~= TSIL.Enums.HealthType.SOUL and healthType ~= TSIL.Enums.HealthType.BLACK then
-        return
-    end
-
-    local heartsAdded = new - old
-    if heartsAdded < 0 then
-        return
-    end
-    print(healthType)
-    print(heartsAdded)
-    if (healthType == 4) then
-        player:AddBlackHearts(-1 * heartsAdded)
-        leviticus:addSoulCharges(player, heartsAdded, healthType)
-    elseif (healthType == 2) then
-        player:AddSoulHearts(-1 * heartsAdded)
-        leviticus:addSoulCharges(player, heartsAdded, healthType)
-    end
-    
-
-end
-function leviticus:newFloor()
-    usedLeviticus = 0
-    return 0
-end
-
-milkshakeMod:AddCallback(TSIL.Enums.CustomCallback.POST_PLAYER_HEALTH_CHANGED, leviticus.OnHealthChanged)
-
-milkshakeMod:AddCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, leviticus.onPickupCollision)
-
-milkshakeMod:AddCallback(ModCallbacks.MC_USE_ITEM, leviticus.onLeviticusUse, enums.Collectibles.LEVITICUS)
-
-milkshakeMod:AddCallback(ModCallbacks.MC_PRE_GET_COLLECTIBLE, leviticus.onItemSpawn)
-
-milkshakeMod:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, leviticus.newFloor)
+milkshakeMod:AddCallback(
+    ModCallbacks.MC_PRE_GET_COLLECTIBLE,
+    Leviticus.onItemSpawn
+)
