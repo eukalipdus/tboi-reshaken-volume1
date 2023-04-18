58d3aaf632cc3a65740bc25d19e7101d486cc560
thicco-catto <cristobal.martin01@gmail.com>
2023-04-18 18:17:38 +0200

    feat: Added chaos orb flash animation

--- milkshake_scripts/items/pick ups/chaosorb.lua
+++ milkshake_scripts/items/pick ups/chaosorb.lua
@@ -10,6 +10,14 @@ local ORBS = {
 }
 
 
+local SPRITESHEET_PER_ORB = {
+    [enums.Cards.AMETHYST_ORB] = "spirit_psychic",
+    [enums.Cards.RUBY_ORB] = "spirit_fire",
+    [enums.Cards.EMERALD_ORB] = "spirit_nature",
+    [enums.Cards.SAPPHIRE_ORB] = "spirit_electricity",
+}
+
+
 ---@param player EntityPlayer
 function ChaosOrb:OnChaosOrbUse(_, player)
     local rng = player:GetCardRNG(enums.Cards.RANDOM_ORB)
@@ -21,9 +29,26 @@ function ChaosOrb:OnChaosOrbUse(_, player)
     )[1]
 
     player:UseCard(orbToUse)
+
+    local sprite = Sprite()
+    sprite:Load("/gfx/chaos_orb_flash.anm2", true)
+    local orbSpritesheet = SPRITESHEET_PER_ORB[orbToUse]
+    sprite:ReplaceSpritesheet(1, "/gfx/items/pick ups/" .. orbSpritesheet .. ".png")
+    sprite:LoadGraphics()
+    sprite:Play("Idle", true)
+    player:AnimatePickup(sprite)
 end
 milkshakeMod:AddCallback(
     ModCallbacks.MC_USE_CARD,
     ChaosOrb.OnChaosOrbUse,
     enums.Cards.RANDOM_ORB
 )
+
+
+-- function onrender()
+
+-- end
+-- milkshakeMod:AddCallback(
+--     ModCallbacks.MC_POST_RENDER,
+--     onrender
+-- )
