d898602316087a1e32b4b77f616cd64c89b25170
thicco-catto <cristobal.martin01@gmail.com>
2023-04-18 17:20:06 +0200

    feat: Added support for custom spirit orb sounds

--- main.lua
+++ main.lua
@@ -50,6 +50,7 @@ require("milkshake_scripts.items.pick ups.emeraldorb")
 require("milkshake_scripts.items.pick ups.fruitheart")
 require("milkshake_scripts.items.pick ups.rubyorb")
 require("milkshake_scripts.items.pick ups.sapphireOrb")
+require("milkshake_scripts.items.pick ups.spiritorbs")
 require("milkshake_scripts.items.pick ups.tatteredpage")
 
 
---
+++ milkshake_scripts/items/pick ups/spiritorbs.lua
@@ -0,0 +1,48 @@
+local SpiritOrbs = {}
+local enums = require("milkshake_scripts.enums")
+
+
+local ORB_SUBTYPES = {
+    [enums.Cards.AMETHYST_ORB] = true,
+    [enums.Cards.EMERALD_ORB] = true,
+    [enums.Cards.RUBY_ORB] = true,
+    [enums.Cards.SAPPHIRE_ORB] = true,
+    [enums.Cards.RANDOM_ORB] = true
+}
+
+
+---@param card EntityPickup
+function SpiritOrbs:OnCardUpdate(card)
+    if not ORB_SUBTYPES[card.SubType] then return end
+
+    local sprite = card:GetSprite()
+
+    if sprite:IsEventTriggered("DropSound") then
+        SFXManager():Stop(SoundEffect.SOUND_SCAMPER)
+        SFXManager():Play(SoundEffect.SOUND_CASH_REGISTER)
+    end
+end
+milkshakeMod:AddCallback(
+    ModCallbacks.MC_POST_PICKUP_UPDATE,
+    SpiritOrbs.OnCardUpdate,
+    PickupVariant.PICKUP_TAROTCARD
+)
+
+
+---@param card EntityPickup
+function SpiritOrbs:OnCardRender(card)
+    if not ORB_SUBTYPES[card.SubType] then return end
+
+    local sprite = card:GetSprite()
+
+    if sprite:IsPlaying("Collect") and sprite:GetFrame() == 0 and
+    SFXManager():IsPlaying(SoundEffect.SOUND_BOOK_PAGE_TURN_12) then
+        SFXManager():Stop(SoundEffect.SOUND_BOOK_PAGE_TURN_12)
+        SFXManager():Play(SoundEffect.SOUND_SHELLGAME)
+    end
+end
+milkshakeMod:AddCallback(
+    ModCallbacks.MC_POST_PICKUP_RENDER,
+    SpiritOrbs.OnCardRender,
+    PickupVariant.PICKUP_TAROTCARD
+)
