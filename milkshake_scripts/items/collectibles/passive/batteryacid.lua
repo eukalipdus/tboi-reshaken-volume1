27b23833a802fee886c0d78560775067f7f291a2
Monwil <monwilsalt@gmail.com>
2023-05-05 00:59:58 +0200

    Various Battery Acid improvements

--- milkshake_scripts/items/collectibles/passive/batteryacid.lua
+++ milkshake_scripts/items/collectibles/passive/batteryacid.lua
@@ -1,24 +1,35 @@
 local batteryAcid = {}
 local enums = milkshakeMod.enums
 
+local itemConfig = Isaac.GetItemConfig()
+local game = Game()
+
+local CHARGETYPE_NORMAL = 0
+
 function batteryAcid:preSpawnCleanAward()
     for i = 0, Game():GetNumPlayers() - 1 do
         local player = Isaac.GetPlayer(i)
         if player:HasCollectible(enums.Collectibles.BATTERY_ACID) then
             local activeItem = player:GetActiveItem(ActiveSlot.SLOT_PRIMARY)
-            local itemConfig = Isaac.GetItemConfig()
-            local maxCharge = itemConfig:GetCollectible(activeItem).MaxCharges
-            
-            if player:GetBatteryCharge() > 0 then
-                
+            if itemConfig:GetCollectible(activeItem).ChargeType == CHARGETYPE_NORMAL then
+                local chargeToAdd
+                local roomShape = game:GetRoom():GetRoomShape()
+                if roomShape == RoomShape.ROOMSHAPE_2x2
+                or roomShape == RoomShape.ROOMSHAPE_LTL
+                or roomShape == RoomShape.ROOMSHAPE_LTR
+                or roomShape == RoomShape.ROOMSHAPE_LBL
+                or roomShape == RoomShape.ROOMSHAPE_LBR then
+                    chargeToAdd = 2
+                else
+                    chargeToAdd = 1
+                end
+                TSIL.Charge.AddCharge(player, ActiveSlot.SLOT_PRIMARY, chargeToAdd)
             end
-
-            local newCharge = player:GetActiveCharge() + 1
-            player:SetActiveCharge(newCharge)
         end
     end
 end
 milkshakeMod:AddCallback(ModCallbacks.MC_PRE_SPAWN_CLEAN_AWARD, batteryAcid.preSpawnCleanAward)
+milkshakeMod:AddCallback(TSIL.Enums.CustomCallback.POST_GREED_MODE_WAVE, batteryAcid.preSpawnCleanAward)
 
 function batteryAcid:entityTakeDmg(entity)
     local player = entity:ToPlayer()
