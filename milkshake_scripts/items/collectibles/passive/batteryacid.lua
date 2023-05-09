b077d056c820e8c70646f67bbc2aceaa7b06a4e0
Monwil <monwilsalt@gmail.com>
2023-05-10 00:03:24 +0200

    Battery Acid: Creep tweaks and Schoolbag compat

--- milkshake_scripts/items/collectibles/passive/batteryacid.lua
+++ milkshake_scripts/items/collectibles/passive/batteryacid.lua
@@ -4,17 +4,19 @@ local enums = MilkshakeVol1.enums
 local itemConfig = Isaac.GetItemConfig()
 local game = Game()
 
+local CREEP_SCALE = 0.5
+local CREEP_DAMAGE = 1.5
 local CREEP_COLOR = Color(0, 0, 0, 1, 0.5, 0.5, 0.1)
 local CREEP_DELAY_MIN_SECONDS = 0.3
 local CREEP_DELAY_MAX_SECONDS = 0.6
 local CREEP_DURATION_SECONDS = 1.5
-local DISCHARGE_TIME_SECONDS = 15
+local DRAIN_TIME_SECONDS = 15
 
 local ONE_SECOND = 30
 local CREEP_DELAY_MIN = CREEP_DELAY_MIN_SECONDS * ONE_SECOND
 local CREEP_DELAY_MAX = CREEP_DELAY_MAX_SECONDS * ONE_SECOND
 local CREEP_DURATION = CREEP_DURATION_SECONDS * ONE_SECOND
-local DISCHARGE_TIME = DISCHARGE_TIME_SECONDS * ONE_SECOND
+local DRAIN_TIME = DRAIN_TIME_SECONDS * ONE_SECOND
 local CHARGETYPE_NORMAL = 0
 
 TSIL.SaveManager.AddPersistentVariable(
@@ -30,32 +32,37 @@ local function BatteryAcidData(player)
     local key = player:GetCollectibleRNG(1):GetSeed()
 
     if not data[key] then
-        data[key] = {DrainTimer = DISCHARGE_TIME, CreepTimer = CREEP_DELAY_MAX}
+        data[key] = {DrainTimer = DRAIN_TIME, CreepTimer = CREEP_DELAY_MAX}
     end
     return data[key]
 end
 
 function batteryAcid:preSpawnCleanAward()
+    local chargeToAdd
+    local roomShape = game:GetRoom():GetRoomShape()
+    if roomShape == RoomShape.ROOMSHAPE_2x2
+    or roomShape == RoomShape.ROOMSHAPE_LTL
+    or roomShape == RoomShape.ROOMSHAPE_LTR
+    or roomShape == RoomShape.ROOMSHAPE_LBL
+    or roomShape == RoomShape.ROOMSHAPE_LBR then
+        chargeToAdd = 2
+    else
+        chargeToAdd = 1
+    end
+
     for i = 0, Game():GetNumPlayers() - 1 do
         local player = Isaac.GetPlayer(i)
         if player:HasCollectible(enums.Collectibles.BATTERY_ACID) then
+            local finalChargeToAdd = chargeToAdd * player:GetCollectibleNum(enums.Collectibles.BATTERY_ACID)
+            local data = BatteryAcidData(player)
+            data.DrainTimer = DRAIN_TIME
             local activeItem = player:GetActiveItem(ActiveSlot.SLOT_PRIMARY)
-            if itemConfig:GetCollectible(activeItem).ChargeType == CHARGETYPE_NORMAL then
-                local chargeToAdd
-                local roomShape = game:GetRoom():GetRoomShape()
-                if roomShape == RoomShape.ROOMSHAPE_2x2
-                or roomShape == RoomShape.ROOMSHAPE_LTL
-                or roomShape == RoomShape.ROOMSHAPE_LTR
-                or roomShape == RoomShape.ROOMSHAPE_LBL
-                or roomShape == RoomShape.ROOMSHAPE_LBR then
-                    chargeToAdd = 2
-                else
-                    chargeToAdd = 1
-                end
-                chargeToAdd = chargeToAdd * player:GetCollectibleNum(enums.Collectibles.BATTERY_ACID)
-                TSIL.Charge.AddCharge(player, ActiveSlot.SLOT_PRIMARY, chargeToAdd)
-                local data = BatteryAcidData(player)
-                data.DrainTimer = DISCHARGE_TIME
+            if activeItem ~= 0 and itemConfig:GetCollectible(activeItem).ChargeType == CHARGETYPE_NORMAL then
+                TSIL.Charge.AddCharge(player, ActiveSlot.SLOT_PRIMARY, finalChargeToAdd)
+            end
+            local secondItem = player:GetActiveItem(ActiveSlot.SLOT_SECONDARY)
+            if activeItem ~= 0 and itemConfig:GetCollectible(secondItem).ChargeType == CHARGETYPE_NORMAL then
+                TSIL.Charge.AddCharge(player, ActiveSlot.SLOT_SECONDARY, finalChargeToAdd)
             end
         end
     end
@@ -70,22 +77,43 @@ function batteryAcid:postPeffectUpdate(player)
         return end
     if player:GetActiveCharge() <= 0 then
         return end
-    local activeItem = player:GetActiveItem()
-    if activeItem == 0 or itemConfig:GetCollectible(activeItem).ChargeType ~= CHARGETYPE_NORMAL then
+
+    local primaryActiveItem = player:GetActiveItem(ActiveSlot.SLOT_PRIMARY)
+    local secondaryActiveItem = player:GetActiveItem(ActiveSlot.SLOT_SECONDARY)
+    local primaryNotSpecial
+    local secondaryNotSpecial
+    if primaryActiveItem ~= 0 then
+        primaryNotSpecial = itemConfig:GetCollectible(primaryActiveItem).ChargeType == CHARGETYPE_NORMAL
+    end
+    if secondaryActiveItem ~= 0 then
+        secondaryNotSpecial = itemConfig:GetCollectible(secondaryActiveItem).ChargeType == CHARGETYPE_NORMAL
+    end
+
+    if
+    not ((primaryNotSpecial and player:GetActiveCharge(ActiveSlot.SLOT_PRIMARY) > 0)
+    or secondaryNotSpecial and player:GetActiveCharge(ActiveSlot.SLOT_SECONDARY) > 0) then
         return end
 
     local data = BatteryAcidData(player)
 
     data.DrainTimer = data.DrainTimer-1
     if data.DrainTimer <= 0 then
-        TSIL.Charge.AddCharge(player, ActiveSlot.SLOT_PRIMARY, -1)
-        data.DrainTimer = DISCHARGE_TIME
+        if primaryNotSpecial then
+            TSIL.Charge.AddCharge(player, ActiveSlot.SLOT_PRIMARY, -1)
+        end
+        if secondaryNotSpecial then
+            TSIL.Charge.AddCharge(player, ActiveSlot.SLOT_SECONDARY, -1)
+        end
+        data.DrainTimer = DRAIN_TIME
     end
     data.CreepTimer = data.CreepTimer-1
     if data.CreepTimer <= 0 then
-        local creep = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.PLAYER_CREEP_GREEN, 0, player.Position, Vector.Zero, player)
+        local creep = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.PLAYER_CREEP_RED, 0, player.Position, Vector.Zero, player):ToEffect()
         creep:ToEffect():SetTimeout(CREEP_DURATION)
         creep.Color = CREEP_COLOR
+        creep.CollisionDamage = CREEP_DAMAGE
+        creep.Scale = CREEP_SCALE
+        creep:Update()
         local rng = player:GetCollectibleRNG(enums.Collectibles.BATTERY_ACID)
         local creepCooldown = rng:RandomInt(CREEP_DELAY_MAX-CREEP_DELAY_MIN) + CREEP_DELAY_MIN
         data.CreepTimer = creepCooldown
