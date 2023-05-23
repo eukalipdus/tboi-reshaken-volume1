3d887bf28780be8ffc45d75383a972a181999adc
eucalyptusJ <88509866+eucalyptusJ@users.noreply.github.com>
2023-05-23 15:33:06 -0400

    feat: Add enums for new spirit orbs

--- milkshake_scripts/enums.lua
+++ milkshake_scripts/enums.lua
@@ -97,7 +97,12 @@ enums.Orbs = {
     ELECTRIC = Isaac.GetCardIdByName("Spirit Of Conductivity"),
     NATURE = Isaac.GetCardIdByName("Spirit Of Druidity"),
     PSYCHIC = Isaac.GetCardIdByName("Spirit Of Clairvoyance"),
-    RANDOM = Isaac.GetCardIdByName("Spirit Of Chaos")
+    RANDOM = Isaac.GetCardIdByName("Spirit Of Chaos"),
+    SALVATION = Isaac.GetCardIdByName("Spirit Of Salvation"),
+    DEMONIC = Isaac.GetCardIdByName("Spirit Of Sacrilege"),
+    BIOHAZARD = Isaac.GetCardIdByName("Spirit Of Virulence"),
+    DEATH = Isaac.GetCardIdByName("Spirit Of Revengance"),
+    WATER = Isaac.GetCardIdByName("Spirit Of Deluge"),
 }
 
 
