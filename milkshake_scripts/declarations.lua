566fa4efd23044961372ce6f00312a98db5fb281
thicco-catto <cristobal.martin01@gmail.com>
2023-07-02 18:32:31 +0200

    feat: Added add callback function declarations for autocomplete

---
+++ milkshake_scripts/declarations.lua
@@ -0,0 +1,26 @@
+--[[
+===============================================
+               ⚠⚠ ATTENTION ⚠⚠
+    THIS FILE SHOULD NEVER BE INCLUDED!!!!!
+
+    IT'S ONLY PURPOSE IS TO GIVE AUTOCOMPLETE
+    TO THE ADD CALLBACK FUNCTIONS
+===============================================
+]]
+
+
+---Adds a callback to be executed
+---@param callback ModCallbacks | CustomCallback | MilkshakeCallbacks
+---@param funct function
+---@param filter any
+function MilkshakeVol1:AddCallback(callback, funct, filter)
+end
+
+
+---Adds a callback to be executed with a given priority
+---@param callback ModCallbacks | CustomCallback | MilkshakeCallbacks
+---@param priority CallbackPriority | integer
+---@param funct function
+---@param filter any
+function MilkshakeVol1:AddPriorityCallback(callback, priority, funct, filter)
+end
