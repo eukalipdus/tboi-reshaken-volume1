4ca8deaf74ed31929c1c804ba3a79d18e55b2a24
eucalyptusJ <88509866+eucalyptusJ@users.noreply.github.com>
2023-07-11 01:19:30 -0400

    fix: Witch doctor mask not checking if the player has the item

--- milkshake_scripts/items/collectibles/passive/witchdoctormask.lua
+++ milkshake_scripts/items/collectibles/passive/witchdoctormask.lua
@@ -37,16 +37,18 @@ local matchingPills = {
 }
 
 function witchDoctorMask:UsePill(pillEffect, player)
-    local colorToEffect = {}
-    for i = 1, PillColor.NUM_STANDARD_PILLS do
-        colorToEffect[Game():GetItemPool():GetPillEffect(i, player)] = i
+    if player:HasCollectible(enums.Collectibles.WITCH_DOCTOR_MASK) then
+        local colorToEffect = {}
+        for i = 1, PillColor.NUM_STANDARD_PILLS do
+            colorToEffect[Game():GetItemPool():GetPillEffect(i, player)] = i
+        end
+        local pillColor = colorToEffect[pillEffect]
+        if TSIL.Pills.IsHorsePill(pillColor) then
+            utility:SetTemporaryPlayerData(player, "IsUsingDoublePowerOrb", true)
+        end
+        player:UseCard(matchingPills[pillColor], UseFlag.USE_NOANIM)
+        --utility:SetTemporaryPlayerData(player, "IsUsingDoublePowerOrb", false) seems like it should be done but could mess with lyra?
     end
-    local pillColor = colorToEffect[pillEffect]
-    if TSIL.Pills.IsHorsePill(pillColor) then
-        utility:SetTemporaryPlayerData(player, "IsUsingDoublePowerOrb", true)
-    end
-    player:UseCard(matchingPills[pillColor], UseFlag.USE_NOANIM)
-    --utility:SetTemporaryPlayerData(player, "IsUsingDoublePowerOrb", false) seems like it should be done but could mess with lyra?
 end
 MilkshakeVol1:AddCallback(ModCallbacks.MC_USE_PILL, witchDoctorMask.UsePill)
 
