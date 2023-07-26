e029e58bc7cda6c8c539f29f878d8c9811a53915
dann <hazrd6@gmail.com>
2023-07-27 01:02:30 +0600

    Revenance orb works fine

--- milkshake_scripts/items/pick ups/revenanceOrb.lua
+++ milkshake_scripts/items/pick ups/revenanceOrb.lua
@@ -3,7 +3,6 @@ local enums = MilkshakeVol1.enums
 local game = Game()
 local sfx = SFXManager()
 
---RevenanceOrb.TombHP = 3
 RevenanceOrb.SkeletonHP = 1
 RevenanceOrb.SkeletonDMG = 3
 
@@ -27,96 +26,44 @@ local function SoundParticle(pos)
 	game:SpawnParticles(pos, EffectVariant.TOOTH_PARTICLE, 3, 1, Color(0.5,0.5,0.5), 100000)
 end
 
-local function DamageTombstone(gravestone)
-	gravestone.HitPoints = gravestone.HitPoints - 1
-	gravestone:SetColor(Color(0.5,0,0),10,1, true, false)
-	SoundParticle(gravestone.Position)
-end
-
-local function CollisionTombstone(gravestone, collider, checkFrame)
-	local colData = collider:GetData()
-	local grbData = gravestone:GetData()
-	if (not grbData.GravetoneTouched or game:GetFrameCount() - grbData.GravetoneTouched > checkFrame) or (not colData.GravetoneTouched or game:GetFrameCount() - colData.GravetoneTouched > checkFrame) then
-		grbData.GravetoneTouched = game:GetFrameCount()
-		colData.GravetoneTouched = game:GetFrameCount()
-		if collider.CollisionDamage > 0 then
-			DamageTombstone(gravestone)
-		end
-	end
-end
-
 ---gravestone update
 function RevenanceOrb:GravestonUpd(gravestone)
 	if gravestone.FrameCount <= 1 then
-		gravestone:AddEntityFlags(EntityFlag.FLAG_NO_KNOCKBACK | EntityFlag.FLAG_NO_PHYSICS_KNOCKBACK)
-		gravestone.GridCollisionClass = EntityGridCollisionClass.GRIDCOLL_NONE
+		gravestone:AddEntityFlags(EntityFlag.FLAG_NO_TARGET | EntityFlag.FLAG_NO_BLOOD_SPLASH| EntityFlag.FLAG_NO_KNOCKBACK | EntityFlag.FLAG_NO_PHYSICS_KNOCKBACK | EntityFlag.FLAG_NO_STATUS_EFFECTS)
+		gravestone.GridCollisionClass = EntityGridCollisionClass.GRIDCOLL_BULLET and EntityGridCollisionClass.GRIDCOLL_GROUND
 		gravestone.EntityCollisionClass = EntityCollisionClass.ENTCOLL_ALL
 	end
-	--[[
-	if gravestone.Variant ~= enums.ENTITY_GENERIC_PROP.GRAVESTONE then return end
-
-	local explosions = Isaac.FindByType(EntityType.ENTITY_EFFECT, EffectVariant.BOMB_EXPLOSION)
-	local mamaMega = Isaac.FindByType(EntityType.ENTITY_EFFECT, EffectVariant.MAMA_MEGA_EXPLOSION)
-	local lasers = Isaac.FindByType(EntityType.ENTITY_LASER)
-	local enemytears =Isaac.FindInRadius(gravestone.Position, 25, EntityPartition.BULLET)
-	gravestone.EntityCollisionClass = EntityCollisionClass.ENTCOLL_ALL
-	--
-	for _, laser in pairs(lasers) do
-		laser = laser:ToLaser()
-		local vectors = laser:GetNonOptimizedSamples()
-		
-		for i=0, #vectors-1 do
-			local pos = vectors:Get(i)
-			if pos:Distance(gravestone.Position) <= 40 and laser.CollisionDamage > 0 and ((not gravestone:GetData().GravetoneTouched or game:GetFrameCount() - gravestone:GetData().GravetoneTouched > 5) or (not laser:GetData().GravetoneTouched or game:GetFrameCount() - laser:GetData().GravetoneTouched > 5)) then
-				gravestone:GetData().GravetoneTouched = game:GetFrameCount()
-				laser:GetData().GravetoneTouched = game:GetFrameCount()
-				DamageTombstone(gravestone)
-			end
+	gravestone.Velocity = Vector.Zero
+	for _, enemytear in pairs(Isaac.FindInRadius(gravestone.Position, 12, EntityPartition.BULLET)) do
+		gravestone:TakeDamage(1, DamageFlag.DAMAGE_INVINCIBLE, EntityRef(enemytear), 1)
+		enemytear:Kill()
+	end
+	for _, tear in pairs(Isaac.FindInRadius(gravestone.Position, 12, EntityPartition.TEAR)) do
+		tear = tear:ToKnife()
+		if tear and (tear.Variant ~= 0 and tear:IsFlying() or tear.Variant == 0 or tear.Variant == 5) then
+			gravestone:TakeDamage(1, DamageFlag.DAMAGE_INVINCIBLE, EntityRef(enemytear), 1)
 		end
 	end
-	
-	for _, enemytear in pairs(enemytears) do
-		if (not gravestone:GetData().GravetoneTouched or game:GetFrameCount() - gravestone:GetData().GravetoneTouched > 15) then
-			gravestone:GetData().GravetoneTouched = game:GetFrameCount()
-			DamageTombstone(gravestone)
-			enemytear:Kill()
-		end
-	end
-
-	if #mamaMega > 0 then
-		gravestone.HitPoints = 0
-	elseif #explosions > 0 then
-		for _, explos in pairs(explosions) do
-			if explos:GetSprite():GetFrame() < 3 and explos.Position:Distance(gravestone.Position) <= 90 * explos.SpriteScale.X then
-				gravestone.HitPoints = 0
-			end
-		end
-	end
-
-	if gravestone.HitPoints > 0 then return end
-	SoundParticle(gravestone.Position)
-	local stageCounter = MilkshakeVol1.utility:GetCurrentChapter()
-	local dmag = RevenanceOrb.SkeletonDMG + stageCounter
-	local skelHP = RevenanceOrb.SkeletonHP + 4*stageCounter
-	local rng = gravestone:GetDropRNG()
-	local randMob = RevenanceOrb.TombMobs[rng:RandomInt(#RevenanceOrb.TombMobs)+1]
-	local mob = Isaac.Spawn(randMob[1], randMob[2], randMob[3], gravestone.Position, Vector.Zero, gravestone.SpawnerEntity)
-	if mob.Type ~= EntityType.ENTITY_EFFECT then
-
-		mob.MaxHitPoints = skelHP
-		mob:GetData().TearDamage = dmag
-		mob:AddEntityFlags(EntityFlag.FLAG_FRIENDLY | EntityFlag.FLAG_CHARM)
-	else
-		mob:ToEffect():SetTimeout(180)
-	end
-	gravestone:Remove()
-	--]]
 end
 MilkshakeVol1:AddCallback(ModCallbacks.MC_PRE_NPC_UPDATE, RevenanceOrb.GravestonUpd, enums.Enemies.GRAVESTONE) --EntityType.ENTITY_GENERIC_PROP)
 
 function RevenanceOrb:GravestonDMG(entity, amount, damageFlags, source, DamageCountdown) -- no way to change amount, blame someone
 	--- do damage effects
-	 SoundParticle(entity.Position)
+	local grbData = entity:GetData()
+
+	if damageFlags & DamageFlag.DAMAGE_EXPLOSION > 0 then
+		return true
+	elseif not grbData.GravetoneTouched or game:GetFrameCount() - grbData.GravetoneTouched > 5 then
+		grbData.CustomDamage = false
+		grbData.GravetoneTouched = game:GetFrameCount()
+		entity:TakeDamage(1, damageFlags, source, DamageCountdown)
+		return false
+	elseif grbData.GravetoneTouched and not grbData.CustomDamage then
+		grbData.CustomDamage = true
+		SoundParticle(entity.Position)
+		return true
+	end
+	return false
 end
 MilkshakeVol1:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, RevenanceOrb.GravestonDMG, enums.Enemies.GRAVESTONE)
 
@@ -139,28 +86,6 @@ function RevenanceOrb:GravestonDeath(gravestone)
 end
 MilkshakeVol1:AddCallback(ModCallbacks.MC_POST_NPC_DEATH, RevenanceOrb.GravestonDeath, enums.Enemies.GRAVESTONE) --E
 
---[[
-function RevenanceOrb:GravestonCollision(gravestone, collider)
-	if gravestone.Variant ~= enums.ENTITY_GENERIC_PROP.GRAVESTONE then return end
-	if collider:ToTear() then
-		CollisionTombstone(gravestone, collider, 15)
-	elseif collider:ToKnife() then
-		CollisionTombstone(gravestone, collider, 5)
-	end
-end
-MilkshakeVol1:AddCallback(ModCallbacks.MC_PRE_NPC_COLLISION, RevenanceOrb.GravestonCollision, EntityType.ENTITY_GENERIC_PROP)
---]]
-
---[[
-function RevenanceOrb:OnNewRoom()
-	local gravestones = Isaac.FindByType(EntityType.ENTITY_GENERIC_PROP, enums.ENTITY_GENERIC_PROP.GRAVESTONE)
-	for _, gravestone in pairs(gravestones) do
-		gravestone:Remove()
-	end
-end
-MilkshakeVol1:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, RevenanceOrb.OnNewRoom)
---]]
-
 function RevenanceOrb:OnRevenanceOrbUse(card, player) -- useFlag
 	local room = game:GetRoom()
 	local rng = player:GetCardRNG(card)
@@ -171,12 +96,9 @@ function RevenanceOrb:OnRevenanceOrbUse(card, player) -- useFlag
 	--- spawn gravestones
 	for _ = 1, tombNum do
 		local pos =  Isaac.GetFreeNearPosition(room:GetRandomPosition(0), 10)
-		local gravestone = Isaac.Spawn(EntityType.ENTITY_GENERIC_PROP, enums.ENTITY_GENERIC_PROP.GRAVESTONE, 0, pos, Vector.Zero, player)
+		gravestone = Isaac.Spawn(enums.Enemies.GRAVESTONE, 0, 0, pos, Vector.Zero, player)
 		gravestone:GetSprite():Play(RevenanceOrb.Anims[rng:RandomInt(#RevenanceOrb.Anims)+1])
 		gravestone:GetSprite().FlipX = 0.5 > rng:RandomFloat()
-		--gravestone.MaxHitPoints = RevenanceOrb.TombHP
-		--gravestone.HitPoints = gravestone.MaxHitPoints
-		--gravestone.EntityCollisionClass = EntityCollisionClass.ENTCOLL_ALL
 	end
 	--- fill pits
 	for gridIndex = 1, room:GetGridSize() do
@@ -198,7 +120,6 @@ function RevenanceOrb:OnRevenanceOrbUse(card, player) -- useFlag
 end
 MilkshakeVol1:AddCallback(enums.Callbacks.ON_ORB_USE, RevenanceOrb.OnRevenanceOrbUse, enums.Orbs.UNDEAD)
 
-
 function RevenanceOrb:OnBoneyMShoot(tear)
 	--- mark that charmed boney shoot bone
 	if tear.SpawnerEntity and RevenanceOrb.Undeads[tear.SpawnerEntity.Type] and tear.SpawnerEntity:GetData().TearDamage and not tear:GetData().BoneyMShootUPD then
@@ -210,8 +131,10 @@ MilkshakeVol1:AddCallback(ModCallbacks.MC_POST_PROJECTILE_UPDATE, RevenanceOrb.O
 function RevenanceOrb:onEnemyTakesDMG(entity, amount, damageFlags, source, DamageCountdown) -- no way to change amount, blame someone
 	--- reapply damage
 	if source.Entity and source.Entity:ToProjectile() and source.Entity:GetData().BoneyMShootUPD then
-		entity:TakeDamage(source.Entity:GetData().BoneyMShootUPD, damageFlags, source, DamageCountdown)
-		source.Entity:GetData().BoneyMShootUPD = nil -- set to nil, so it doesn't loop
+		local dmg = source.Entity:GetData().BoneyMShootUPD
+		source.Entity:GetData().BoneyMShootUPD = nil
+		entity:TakeDamage(dmg, damageFlags, source, DamageCountdown)
+		 -- set to nil, so it doesn't loop
 		return false
 	end
 end
