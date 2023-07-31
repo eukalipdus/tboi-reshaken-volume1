a599261984aa86bec80b4d4d607b8352d601e9cd
eucalyptusJ <88509866+eucalyptusJ@users.noreply.github.com>
2023-07-31 01:15:49 -0400

    fix: Check for pickup variant and not entity type for sprite switch

--- milkshake_scripts/items/collectibles/passive/witchdoctormask.lua
+++ milkshake_scripts/items/collectibles/passive/witchdoctormask.lua
@@ -79,7 +79,7 @@ function witchDoctorMask:PostPickupUpdate(pickup)
     for i = 0, Game():GetNumPlayers() - 1 do
         local player = Isaac.GetPlayer(i)
         if player:HasCollectible(enums.Collectibles.WITCH_DOCTOR_MASK)
-        and pickup.Type == PickupVariant.PICKUP_PILL then
+        and pickup.Variant == PickupVariant.PICKUP_PILL then
             if not utility:GetData(pickup, "SpiritPillSprite") then
                 if pickup.SubType < FF_PILL_BEGIN then
                     pickup:GetSprite():ReplaceSpritesheet(0, "gfx/items/pick ups/spirit pills ground.png")
