82f85125f63e67121781ccc34d8053e23809bf59
Maya <mayacampa06@gmail.com>
2023-08-18 14:54:33 -0700

    holy orb balance attempt 2

--- milkshake_scripts/items/pick ups/holyorb.lua
+++ milkshake_scripts/items/pick ups/holyorb.lua
@@ -30,8 +30,8 @@ local GRID_DESTRUCTION_WHITELIST = TSIL.Utils.Tables.ConstructDictionaryFromTabl
 })
 
 local function getLaserDamagePerTick()
-    local stage = TSIL.Stage.GetEffectiveStage()
-    return 2.8 * math.max(math.ceil(stage / 2.5), 1)
+    local stage = MilkshakeVol1.utility:GetCurrentChapter()
+    return 2.93 * math.max(math.ceil(stage / 2.5), 1)
 end
 
 local function vLerp(vec1, vec2, percent)
