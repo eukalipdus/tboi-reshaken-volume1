811ae9a628b55d4cf83d4c49f842e76e0e920eb8
dan2 <hazrd6@gmail.com>
2023-08-31 09:49:23 +0600

    fix: unholy orb - collision brim mark

--- milkshake_scripts/items/pick ups/unholyorb.lua
+++ milkshake_scripts/items/pick ups/unholyorb.lua
@@ -1,6 +1,9 @@
 local UnholyOrb = {}
 local game = Game()
 
+local enums = MilkshakeVol1.enums
+local utility = MilkshakeVol1.utility
+
 -- some thoughts
 -- killing beggars is difficult stuff, cause beggar is "slot machine", so it must also destroy slot machines, cause there is mod beggars
 
@@ -12,6 +15,7 @@ UnholyOrb.DamageMultiplier = 5
 
 
 
+
 function UnholyOrb.GetNearestEnemy(basePos)
 	--- get near enemy's position, else return basePos position
 	local positionsTable = {}
@@ -19,16 +23,31 @@ function UnholyOrb.GetNearestEnemy(basePos)
 	for _, enemy in pairs(enemies) do
 		if enemy:ToNPC() and not enemy:HasEntityFlags(EntityFlag.FLAG_FRIENDLY) and enemy.Type ~= EntityType.ENTITY_FIREPLACE then
 			--positionsTable.insert(enemy.Position, basePos:Distance(enemy.Position))
-			positionsTable.insert(enemy)
+			table.insert(positionsTable, enemy)
         end
 	end
 	local slots = Isaac.FindByType(EntityType.ENTITY_SLOT)
 	for _, slot in pairs(slots) do
 		--positionsTable.insert(slot.Position, basePos:Distance(slot.Position))
-		positionsTable.insert(slot)
+		table.insert(positionsTable, slot)
 	end
 	return positionsTable
 end
+
+
+function UnholyOrb:onPEffectUpdate(player)
+	if utility:GetData(player, "UsedUnholyOrb") then
+		if game:GetRoom():GetFrameCount() == 1 then
+			utility:SetData(player, "UsedUnholyOrb", nil)
+		else -- if positionsTable has elements
+			-- move player to targets by X speed
+			--player.Velocity =
+			-- remove element from positionsTable
+			-- if #positionsTable == 0 then set utility:SetData(player, "UsedUnholyOrb", nil)
+		end
+	end
+end
+MilkshakeVol1:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, UnholyOrb.onPEffectUpdate)
 
 
 function UnholyOrb:enemyUpd(enemy)
@@ -42,11 +61,13 @@ end
 MilkshakeVol1:AddCallback(ModCallbacks.MC_PRE_NPC_UPDATE, UnholyOrb.enemyUpd)
 
 function UnholyOrb:onPlayerCollision(player, collider)
+	if not utility:GetData(player, "UsedUnholyOrb") then return end
 	if not collider:ToNPC() then return end
 	if collider:HasEntityFlags(EntityFlag.FLAG_FRIENDLY) then return end
     local enemy = collider:ToNPC()
 	game:ShakeScreen(5)
-    enemy:TakeDamage(UnholyOrb.DamageMultiplier + MilkshakeVol1.utility:GetCurrentChapter(), DamageFlag.DAMAGE_CRUSH, EntityRef(player), 1)
+	local damage = UnholyOrb.DamageMultiplier + utility:GetCurrentChapter()
+    enemy:TakeDamage(damage, DamageFlag.DAMAGE_CRUSH, EntityRef(player), 1)
 	enemy:AddEntityFlags(EntityFlag.FLAG_BLEED_OUT | EntityFlag.FLAG_BRIMSTONE_MARKED | EntityFlag.FLAG_EXTRA_GORE)
 	enemy:GetData().UnholyOrbFlag = true
 end
@@ -55,21 +76,18 @@ MilkshakeVol1:AddCallback(ModCallbacks.MC_PRE_PLAYER_COLLISION, UnholyOrb.onPlay
 ---@param player EntityPlayer
 function UnholyOrb:OnUnholyOrbUse(_, player)
 
+	player:UseActiveItem(CollectibleType.COLLECTIBLE_DARK_ARTS)
+
     local room = game:GetRoom()
     UnholyOrb.StartingPos = player.Position -- return to this position
     local positionsTable = UnholyOrb.GetNearestEnemy(player.Position)
 
 	if #positionsTable == 0 then
-		player:UseActiveItem(CollectibleType.COLLECTIBLE_DARK_ARTS)
-	else
+		utility:SetData(player, "UsedUnholyOrb", true)
 		player:UseActiveItem(CollectibleType.COLLECTIBLE_PAUSE, UseFlag.USE_NOANIM | UseFlag.USE_NOANNOUNCER | UseFlag.USE_MIMIC)
 		local pentagram = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.PENTAGRAM_BLACKPOWDER, 0, room:GetCenterPos(), Vector.Zero, player):ToEffect()
 	    pentagram:GetData().UnholyOrbFlag = true -- remove after dash
 		pentagram:SetTimeout(-1)
 	end
 end
-MilkshakeVol1:AddCallback(
-    MilkshakeVol1.enums.Callbacks.ON_ORB_USE,
-    UnholyOrb.OnUnholyOrbUse,
-    MilkshakeVol1.enums.Orbs.UNHOLY
-)
+MilkshakeVol1:AddCallback(enums.Callbacks.ON_ORB_USE, UnholyOrb.OnUnholyOrbUse, enums.Orbs.UNHOLY)
