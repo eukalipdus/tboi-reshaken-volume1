be2057082c664ac4e3bc36d25a3f8083dfc97349
eucalyptusJ <88509866+eucalyptusJ@users.noreply.github.com>
2023-09-13 22:48:16 -0400

    chore: Delete unused file

--- main.lua
+++ main.lua
@@ -99,7 +99,6 @@ include("milkshake_scripts.wisps.brendawaterwisp")
 include("milkshake_scripts.wisps.globininabucket")
 include("milkshake_scripts.wisps.goldenshovel")
 include("milkshake_scripts.wisps.leviticus")
-include("milkshake_scripts.wisps.prismaticdice")
 
 --Enemies
 include("milkshake_scripts.enemies.beerhead")
--- milkshake_scripts/wisps/prismaticdice.lua
+++
@@ -1,9 +0,0 @@
-local prismaticDice = {}
-local enums = MilkshakeVol1.enums
-local utility = MilkshakeVol1.utility
-
-local WISP_GRID_COLLISION_POINTS = 11
-
-
-
-return prismaticDice
