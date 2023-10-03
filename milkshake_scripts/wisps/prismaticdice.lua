61e453f66fb25920224c35f47e54f6e88e72c970
eucalyptusJ <88509866+eucalyptusJ@users.noreply.github.com>
2023-10-03 17:00:13 -0400

    fix: Prismatic Dice wisps no longer split non fetus bombs

--- milkshake_scripts/wisps/prismaticdice.lua
+++ milkshake_scripts/wisps/prismaticdice.lua
@@ -65,7 +65,9 @@ function prismaticDice:FamiliarUpdate(familiar)
 
     local bombsInRoom = TSIL.Entities.GetEntities(EntityType.ENTITY_BOMB)
     for _, bomb in ipairs(bombsInRoom) do
+        bomb = bomb:ToBomb()
         if bomb.Position:Distance(familiar.Position, bomb.Position) < 10
+        and bomb.IsFetus
         and not utility:GetData(bomb, "PrismaticWispBomb") then
             bomb:Remove()
 
