bf2f3c0a4f4e31d14d7cbeedbd8b284fdc36c1ee
eucalyptusJ <88509866+eucalyptusJ@users.noreply.github.com>
2023-10-17 19:22:15 -0400

    fix: Shard overlays now render on post_grid_entity_init

--- milkshake_scripts/shardrockoverlay.lua
+++ milkshake_scripts/shardrockoverlay.lua
@@ -6,7 +6,9 @@ local shardTrinkets = {
     enums.Trinkets.AMBER_SHARD,
 }
 
-function shardRockOverlay:PostNewRoom()
+function shardRockOverlay:PostGridEntityInit()
+    local emptyEffects = TSIL.EntitySpecific.GetEffects(enums.Effects.EFFECT_REPLACER)
+    if #emptyEffects > 1 then return end
     for _, trinket in ipairs(shardTrinkets) do
          if utility:DoesTrinketExist(trinket) then
             TSIL.EntitySpecific.SpawnEffect(enums.Effects.EFFECT_REPLACER, 0, Vector.Zero, Vector.Zero, Isaac.GetPlayer(0), Random() + 1)
@@ -14,6 +16,6 @@ function shardRockOverlay:PostNewRoom()
          end
     end
 end
-MilkshakeVol1:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, shardRockOverlay.PostNewRoom)
+MilkshakeVol1:AddCallback(TSIL.Enums.CustomCallback.POST_GRID_ENTITY_INIT, shardRockOverlay.PostGridEntityInit)
 
 return shardRockOverlay
