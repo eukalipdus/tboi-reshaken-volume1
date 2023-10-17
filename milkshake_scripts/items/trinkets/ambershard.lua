6ee94fe50fc41af8be5c31a93bab318ec788d410
eucalyptusJ <88509866+eucalyptusJ@users.noreply.github.com>
2023-10-17 00:13:34 -0400

    fix: Tinted rocks getting the crystal overlay when no player had amber shard

--- milkshake_scripts/items/trinkets/ambershard.lua
+++ milkshake_scripts/items/trinkets/ambershard.lua
@@ -8,6 +8,7 @@ end
 MilkshakeVol1:AddCallback(TSIL.Enums.CustomCallback.POST_GRID_ENTITY_BROKEN, amberShard.postGridEntityBroken)
 
 function amberShard:PostEffectRender()
+    if not utility:DoesTrinketExist(enums.Trinkets.AMBER_SHARD) then return end
     local tintedRocks = TSIL.GridEntities.GetGridEntities(GridEntityType.GRID_ROCKT)
     for _, gridEntity in ipairs(tintedRocks) do
         if gridEntity.State ~= 2 then
