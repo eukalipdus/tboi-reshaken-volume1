382cdbb8c60766cb934568db556e80afe9366237
eucalyptusJ <88509866+eucalyptusJ@users.noreply.github.com>
2023-11-12 19:00:35 -0500

    feat: Stalagmites now do a set amount of damage and if an enemy cannot be found, spawn in a random location

--- milkshake_scripts/items/pick ups/rockorb.lua
+++ milkshake_scripts/items/pick ups/rockorb.lua
@@ -9,10 +9,10 @@ local BIG_MIN = 7
 local BIG_MAX = 9
 local KILL_FRAME = 7
 local KILL_RADIUS = 20
+local SHAKE_TIMEOUT = 25
+local STALAGMITE_DMG = 200
 
-function rockOrb:OnOrbUse(orb, player, _, isLyra)
-    if orb ~= enums.Orbs.ROCK then return end
-
+local function SpawnRandomStalagmites(player)
     local enemies = TSIL.EntitySpecific.GetNPCs(nil, nil, nil, true)
 
     enemies = TSIL.Utils.Tables.Filter(enemies, function (_, enemy)
@@ -32,32 +32,49 @@ function rockOrb:OnOrbUse(orb, player, _, isLyra)
 
     local stalagmiteCount = TSIL.Random.GetRandomInt(min, max)
 
-    for _ = 1, stalagmiteCount do
-        local target = TSIL.Random.GetRandomElementsFromTable(enemies)
-        local stalagmite = TSIL.EntitySpecific.SpawnEffect(
-            enums.Effects.STALAGMITE,
-            0,
-            target[1].Position,
-            Vector.Zero,
-            player
-        )
-
+    for itr = 1, stalagmiteCount do
+        local stalagmite
+        if itr >= #enemies then
+            stalagmite = TSIL.EntitySpecific.SpawnEffect(
+                enums.Effects.STALAGMITE,
+                0,
+                Isaac.GetRandomPosition(),
+                Vector.Zero,
+                player
+            )
+        else
+            local target = TSIL.Random.GetRandomElementsFromTable(enemies)
+            stalagmite = TSIL.EntitySpecific.SpawnEffect(
+                enums.Effects.STALAGMITE,
+                0,
+                target[1].Position,
+                Vector.Zero,
+                player
+            )
+        end
         local sprite = stalagmite:GetSprite()
         sprite:Play("Windup")
-        utility:SetData(stalagmite, "Target", target[1])
+        --utility:SetData(stalagmite, "Target", target[1])
     end
+end
+
+function rockOrb:OnOrbUse(orb, player, _, isLyra)
+    if orb ~= enums.Orbs.ROCK then return end
+
+    Game():ShakeScreen(SHAKE_TIMEOUT)
+
+    TSIL.Utils.Functions.RunInFrames(SpawnRandomStalagmites, 15, player)
 end
 MilkshakeVol1:AddCallback(enums.Callbacks.ON_ORB_USE, rockOrb.OnOrbUse)
 
 function rockOrb:PostEffectUpdate(stalagmite)
     local sprite = stalagmite:GetSprite()
-    local target = utility:GetData(stalagmite, "Target")
-
+    --local target = utility:GetData(stalagmite, "Target")
     if sprite:IsFinished("Windup") then
         sprite:Play("Appear")
         TSIL.Utils.Functions.RunInFrames(function ()
             SFXManager():Play(SoundEffect.SOUND_ROCK_CRUMBLE)
-        end, 5, {})
+        end, 5)
     end
 
     if sprite:IsFinished("Appear") then
@@ -65,9 +82,9 @@ function rockOrb:PostEffectUpdate(stalagmite)
     end
 
     if sprite:IsPlaying("Appear") and sprite:GetFrame() == KILL_FRAME then
-        local toKill = Isaac.FindInRadius(stalagmite.Position, KILL_RADIUS, EntityPartition.ENEMY)
-        for _, enemy in ipairs(toKill) do
-            enemy:Kill()
+        local target = Isaac.FindInRadius(stalagmite.Position, KILL_RADIUS, EntityPartition.ENEMY)
+        for _, enemy in ipairs(target) do
+            enemy:TakeDamage(STALAGMITE_DMG, 0, EntityRef(stalagmite), 0)
         end
         TSIL.Utils.Functions.RunInFrames(function ()
             sprite:Play("Disappear")
