a1dcb11615ce9f07403e3ac8eda808ae245ca5fb
Kattack <29678669+Kattacka@users.noreply.github.com>
2023-11-14 14:24:54 -0500

    Fix immortal heart leviticus hopefullyt

--- milkshake_scripts/modcompatibility/immortalHearts.lua
+++ milkshake_scripts/modcompatibility/immortalHearts.lua
@@ -1,16 +1,16 @@
 MilkshakeVol1:AddModCompatibility("ComplianceImmortal", function ()
     if EID then
-        EID:AddCollectible(
+        EID:addCollectible(
             MilkshakeVol1.enums.Collectibles.LEVITICUS,
             "{{SoulHeart}} Must be charged by picking up soul hearts" ..
-            "#{{ImmortalHeart}} +1 Immnortal Heart" ..
+            "#{{ImmortalHeart}} +1 Immortal Heart" ..
             "#{{AngelRoom}} Using the item before a boss fight makes the boss reward an angel item" ..
             "#{{DevilRoom}} The angel item will cost money if a devil deal was taken previously",
             "Leviticus",
             "en_us"
         )
 
-        EID:AddCollectible(
+        EID:addCollectible(
             MilkshakeVol1.enums.Collectibles.LEVITICUS,
             "{{SoulHeart}} Debe ser cargado usando corazones de alma" ..
             "#{{ImmortalHeart}} +1 Corazón Inmortal" ..
@@ -20,10 +20,10 @@ MilkshakeVol1:AddModCompatibility("ComplianceImmortal", function ()
             "spa"
         )
 
-        EID:AddCollectible(
+        EID:addCollectible(
             MilkshakeVol1.enums.Collectibles.LEVITICUS,
             "{{SoulHeart}} Must be charged by picking up soul hearts" ..
-            "#{{ImmortalHeart}} +1 Immnortal Heart" ..
+            "#{{ImmortalHeart}} +1 Immortal Heart" ..
             "#{{AngelRoom}} Using the item before a boss fight makes the boss reward an angel item" ..
             "#{{DevilRoom}} The angel item will cost money if a devil deal was taken previously",
             "Leviticus",
