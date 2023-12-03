0f16bcf9135f3649fe7e4feb709ae93156317ab0
eucalyptusJ <88509866+eucalyptusJ@users.noreply.github.com>
2023-12-03 14:54:19 -0500

    feat: Implement Sickle Cell locust

--- main.lua
+++ main.lua
@@ -111,6 +111,7 @@ include("milkshake_scripts.wisps.prismaticdice")
 --Locusts
 include("milkshake_scripts.locusts.prismaticdice")
 include("milkshake_scripts.locusts.celestialmirror")
+include("milkshake_scripts.locusts.sicklecell")
 
 --Enemies
 include("milkshake_scripts.enemies.beerhead")
