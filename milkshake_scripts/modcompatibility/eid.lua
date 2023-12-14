1e52b04953e1d77841dcc8ecb5db7eb58d32dc6b
Kattack <29678669+Kattacka@users.noreply.github.com>
2023-12-13 22:36:02 -0500

    Added EIDs for all Locusts, Wisps, and Belial Birthright synergies
    Also added milkshake and leviticus locusts, fixed up some locust and wisp stats, and fixed up locust sprites.
    also added fixed shattered challenge room theme

--- milkshake_scripts/modcompatibility/eid.lua
+++ milkshake_scripts/modcompatibility/eid.lua
@@ -27,7 +27,7 @@ MilkshakeVol1:AddModCompatibility("EID", function ()
 
     local spr = Sprite()
     spr:Load("gfx/spirit_chaos.anm2", true)
-    EID:addIcon("SpiritOrb", "EIDIcon", 1, 16, 16, 6, 6, spr)
+    EID:addIcon("SpiritOrb", "EIDIcon", 1, 6, 6, 3, 6, spr)
 
     local spr = Sprite()
     spr:Load("gfx/fruit_heart.anm2", true)
@@ -71,4 +71,27 @@ MilkshakeVol1:AddModCompatibility("EID", function ()
         end
         end
     end
+
+    -- Book of Virtues
+    EID.descriptions["en_us"].bookOfVirtuesWisps[enums.Collectibles.GLOBIN_IN_A_BUCKET] = "Middle ring wisp#50% chance to reform on death"
+    EID.descriptions["en_us"].bookOfVirtuesWisps[enums.Collectibles.PRISMATIC_DICE] = "Middle ring wisp#Cannot shoot tears#Splits Isaac's tears into 4 {{Collectible528}} angelic prism tears"
+    EID.descriptions["en_us"].bookOfVirtuesWisps[enums.Collectibles.GOLDEN_SHOVEL] = "Middle ring wisp#High HP wisp#10% chance for {{Collectible202}} Midas' Touch tears"
+    EID.descriptions["en_us"].bookOfVirtuesWisps[enums.Collectibles.EMPTY_SLOT] = "Inner ring wisp#Low HP wisp"
+    EID.descriptions["en_us"].bookOfVirtuesWisps[enums.Collectibles.LEVITICUS] = "Inner ring wisp#High HP wisp#+10% {{AngelRoom}} Angel Room chance per Leviticus wisp"
+    EID.descriptions["en_us"].bookOfVirtuesWisps[enums.Collectibles.SHATTERED_ORB] = "Upon shattering, spawns 3 random elemental wisps of varying effects"
+
+    -- Book of Belial Judas Birthright
+    EID.descriptions["en_us"].bookOfBelialBuffs[enums.Collectibles.SHATTERED_ORB] = "50% chance to replace spawned {{SpiritOrb}} Spirit of Chaos with Spirit of Sacrilege"
+    EID.descriptions["en_us"].bookOfBelialBuffs[enums.Collectibles.MIRROR_KEY] = "↑ {{Damage}} +2.5 Damage while in the mirror world"
+    EID.descriptions["en_us"].bookOfBelialBuffs[enums.Collectibles.UNCHARGED_MIRROR_KEY] = "↑ Damage applies in the mirror world"
+    EID.descriptions["en_us"].bookOfBelialBuffs[enums.Collectibles.EMPTY_SLOT] = "↑ {{Damage}} +0.066 Damage per coin inserted while held"
+    EID.descriptions["en_us"].bookOfBelialBuffs[enums.Collectibles.PRISMATIC_DICE] = "30% chance for split items to be {{DevilRoom}} Devil items"
+
+    -- Abyss
+    EID.descriptions["en_us"].abyssSynergies[enums.Collectibles.MILKSHAKE] = "Large, fast, pink locust that deals 2x Isaac's damage"
+    EID.descriptions["en_us"].abyssSynergies[enums.Collectibles.FIRECRACKER_ROSE] = "Green, burning locust that has a 10% chance to inflict {{Burning}} Kabloom"
+    EID.descriptions["en_us"].abyssSynergies[enums.Collectibles.PRISMATIC_DICE] = "Pink, glowing locust that has a 10% chance to split enemies into two weaker enemies on contact"
+    EID.descriptions["en_us"].abyssSynergies[enums.Collectibles.DADS_MITT] = "Baseball locust that follows Isaac's movement momentum"
+    EID.descriptions["en_us"].abyssSynergies[enums.Collectibles.SICKLE_CELL] = "Red locust that inflicts {{BleedingOut}} Bleeding"
+    EID.descriptions["en_us"].abyssSynergies[enums.Collectibles.LEVITICUS] = "Blue, glowing locust that can spawn beams of light that deal deal 3x Isaac's damage"
 end)
