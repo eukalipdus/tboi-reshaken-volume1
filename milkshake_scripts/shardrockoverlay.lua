116bc4fe467e1cfd8e5c5e27ecf4db190792e705
eucalyptusJ <88509866+eucalyptusJ@users.noreply.github.com>
2023-12-16 14:32:14 -0500

    chore: Update shard rock overlay stuff with new loop and TSIL function

--- milkshake_scripts/shardrockoverlay.lua
+++ milkshake_scripts/shardrockoverlay.lua
@@ -31,12 +31,11 @@ local shardTrinketNames = {
 function shardRockOverlay:PostGridEntityInit()
     local emptyEffects = TSIL.EntitySpecific.GetEffects(enums.Effects.EFFECT_REPLACER)
     if #emptyEffects > 1 then return end
-    for _, trinket in ipairs(shardTrinkets) do
-        local player = utility:DoesTrinketExist(trinket)
-         if player then
-            TSIL.EntitySpecific.SpawnEffect(enums.Effects.EFFECT_REPLACER, 0, Vector.Zero, Vector.Zero, player, Random() + 1)
+    for _, trinketId in ipairs(shardTrinkets) do
+        if TSIL.Players.DoesAnyPlayerHasTrinket(trinketId) then
+            TSIL.EntitySpecific.SpawnEffect(enums.Effects.EFFECT_REPLACER, 0, Vector.Zero, Vector.Zero, nil, Random() + 1)
             break
-         end
+        end
     end
 end
 MilkshakeVol1:AddCallback(TSIL.Enums.CustomCallback.POST_GRID_ENTITY_INIT, shardRockOverlay.PostGridEntityInit)
