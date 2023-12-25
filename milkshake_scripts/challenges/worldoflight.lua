52a1699a5f7ef1ae14b166ece7dce4ff099f1b1c
eucalyptusJ <88509866+eucalyptusJ@users.noreply.github.com>
2023-12-25 12:44:13 -0500

    feat: Pickups are now split on first room entry

--- milkshake_scripts/challenges/worldoflight.lua
+++ milkshake_scripts/challenges/worldoflight.lua
@@ -1,8 +1,5 @@
 local worldOfLight = {}
 local enums = MilkshakeVol1.enums
-local utility = MilkshakeVol1.utility
-
-local ignore = 0
 
 function worldOfLight:PostPlayerInit(player)
     if Game().Challenge == enums.Challenges.WORLD_OF_LIGHT then
@@ -13,23 +10,20 @@ function worldOfLight:PostPlayerInit(player)
 end
 MilkshakeVol1:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, worldOfLight.PostPlayerInit)
 
-function worldOfLight:PostPickupInit(pickup)
-    if ignore ~= 0
-    or Game().Challenge ~= enums.Challenges.WORLD_OF_LIGHT
-    or pickup.Variant ~= PickupVariant.PICKUP_COLLECTIBLE
-    or utility:DidEntityExist() then return end
-    local quality = Isaac.GetItemConfig():GetCollectible(pickup.SubType).Quality
-    local newCollectibleID
-    TSIL.Utils.Functions.RunInFrames(function ()
-        pickup:Remove()
-        ignore = ignore + 1
-        MilkshakeVol1.API.SplitCollectible(Isaac.GetPlayer(), pickup, quality-1, newCollectibleID, quality)
-        SFXManager():Play(SoundEffect.SOUND_MIRROR_EXIT)
+function worldOfLight:PostNewRoom()
+    if not Game():GetRoom():IsFirstVisit()
+    or Game().Challenge ~= enums.Challenges.WORLD_OF_LIGHT then return end
+    local collectibles = TSIL.PickupSpecific.GetCollectibles()
+    for _, currentCollectible in ipairs(collectibles) do
+        local quality = Isaac.GetItemConfig():GetCollectible(currentCollectible.SubType).Quality
+        local newCollectibleID
         TSIL.Utils.Functions.RunInFrames(function ()
-            ignore = ignore - 1
+            currentCollectible:Remove()
+            MilkshakeVol1.API.SplitCollectible(Isaac.GetPlayer(), currentCollectible:ToPickup(), quality-1, newCollectibleID, quality)
+            SFXManager():Play(SoundEffect.SOUND_MIRROR_EXIT)
         end, 1, {})
-    end, 2, {})
+    end
 end
-MilkshakeVol1:AddCallback(ModCallbacks.MC_POST_PICKUP_INIT, worldOfLight.PostPickupInit)
+MilkshakeVol1:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, worldOfLight.PostNewRoom)
 
 return worldOfLight
