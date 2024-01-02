27c447e42d52d697c752a9d4bd95ed5aa9af5563
Kattack <29678669+Kattacka@users.noreply.github.com>
2024-01-01 22:02:22 -0500

    Fixed tBeth not being able to pick up fruit hearts when at max soul hearts

--- milkshake_scripts/items/pick ups/fruitheart.lua
+++ milkshake_scripts/items/pick ups/fruitheart.lua
@@ -9,7 +9,7 @@ function FruitHeart:OnPlayerCollision(player, collider)
     if collider.Variant ~= PickupVariant.PICKUP_HEART then return end
     if collider.SubType ~= enums.Hearts.FRUIT_HEART then return end
 
-    if player:CanPickRedHearts() and player:GetPlayerType() ~= PlayerType.PLAYER_BETHANY_B then
+    if player:CanPickRedHearts() and not (player:GetPlayerType() == PlayerType.PLAYER_BETHANY_B and player:GetSoulHearts() < 24) then
         player:AddHearts(1)
     else
         player:AddSoulHearts(1)
