00b333c217f07a9d9174459c3791e8b84ea6d59c
eucalyptusJ <88509866+eucalyptusJ@users.noreply.github.com>
2024-01-05 16:17:58 -0500

    feat: World of Light now removes a random item every minute

--- milkshake_scripts/challenges/worldoflight.lua
+++ milkshake_scripts/challenges/worldoflight.lua
@@ -1,11 +1,71 @@
 local worldOfLight = {}
 local enums = MilkshakeVol1.enums
 
+local INTERVAL_SECONDS = 60
+local ONE_SECOND = 30
+
+local RENDER_X = 25
+local RENDER_Y = 230
+local SCALE_X = 1.3
+local SCALE_Y = 1.3
+local TIMES_CAN_FAIL = 100
+local PUSH_ABOVE_ISAAC = Vector(0, -25)
+
+local itemBlacklist = {
+    CollectibleType.COLLECTIBLE_KEY_PIECE_1,
+    CollectibleType.COLLECTIBLE_KEY_PIECE_2,
+    CollectibleType.COLLECTIBLE_POLAROID,
+    CollectibleType.COLLECTIBLE_NEGATIVE,
+}
+
+local renderItems = {}
+
+--- If there is an item, such as a story one that SHOULD NOT be removed during this challenge
+---@param collectibleId integer
+function MilkshakeVol1.AddToWorldOfLightBlacklist(collectibleId)
+    table.insert(itemBlacklist, collectibleId)
+end
+
+--- Get a collectible sprite and make it play a fading animation above Isaac
+---@param player EntityPlayer
+---@param collectibleId integer
+local function AnimateCollectibleLoss(player, collectibleId)
+    local collectibleConfig = Isaac:GetItemConfig():GetCollectible(collectible)
+    local sprite = Sprite()
+    sprite:Load("gfx/wol_collectible.anm2", true)
+    sprite:ReplaceSpritesheet(1, collectibleConfig.GfxFileName)
+    sprite:LoadGraphics()
+    sprite:Play("Fade", true)
+    player:AnimateSad()
+    table.insert(renderItems, {Sprite = sprite, Player = player})
+end
+
+--- Removes a random collectible from a player that is not blacklisted
+---@param player EntityPlayer
+---@return boolean - true if removed, false otherwise
+local function RemoveRandomCollectible(player)
+    local inventory = TSIL.Players.GetPlayerInventory(player, TSIL.Enums.InventoryType.COLLECTIBLE)
+    if #inventory == 0 then return end
+    local rng = player:GetDropRNG()
+    local roll
+    local itr = 0
+
+    repeat roll = TSIL.Random.GetRandomInt(1, #inventory, rng)
+        itr = itr + 1
+        if itr == TIMES_CAN_FAIL then return false end
+    until not TSIL.Utils.Tables.IsIn(itemBlacklist, inventory[roll].Id)
+ 
+    AnimateCollectibleLoss(player, inventory[roll].Id)
+    player:RemoveCollectible(inventory[roll].Id)
+    return true
+end
+
 function worldOfLight:PostPlayerInit(player)
     if Game().Challenge == enums.Challenges.WORLD_OF_LIGHT then
         player:AddCollectible(enums.Collectibles.PRISMATIC_DICE)
         local prismaticDiceCharges = Isaac.GetItemConfig():GetCollectible(enums.Collectibles.PRISMATIC_DICE).MaxCharges
         player:SetActiveCharge(prismaticDiceCharges)
+        TSIL.SaveManager.AddPersistentVariable(MilkshakeVol1, "WoLItemRemovalTimer", INTERVAL_SECONDS, TSIL.Enums.VariablePersistenceMode.RESET_RUN)
     end
 end
 MilkshakeVol1:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, worldOfLight.PostPlayerInit)
@@ -25,5 +85,39 @@ function worldOfLight:PostNewRoom()
     end
 end
 MilkshakeVol1:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, worldOfLight.PostNewRoom)
+
+function worldOfLight:PostPEffectUpdate()
+    if Game().Challenge ~= enums.Challenges.WORLD_OF_LIGHT then return end
+    if Game():GetFrameCount() % ONE_SECOND == 0 and Game():GetFrameCount() > ONE_SECOND then
+        local timer = TSIL.SaveManager.GetPersistentVariable(MilkshakeVol1, "WoLItemRemovalTimer")
+        if not timer then return end
+
+        timer = timer - 1
+        if timer <= 0 then
+            TSIL.SaveManager.SetPersistentVariable(MilkshakeVol1, "WoLItemRemovalTimer", INTERVAL_SECONDS)
+            for _, player in ipairs(TSIL.Players.GetPlayers()) do
+                RemoveRandomCollectible(player)
+            end
+        else
+            TSIL.SaveManager.SetPersistentVariable(MilkshakeVol1, "WoLItemRemovalTimer", timer)
+        end
+    end
+end
+MilkshakeVol1:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, worldOfLight.PostPEffectUpdate)
+
+function worldOfLight:PostRender()
+    if Game().Challenge ~= enums.Challenges.WORLD_OF_LIGHT then return end
+    local timer = TSIL.SaveManager.GetPersistentVariable(MilkshakeVol1, "WoLItemRemovalTimer")
+    Isaac.RenderScaledText(timer, RENDER_X, RENDER_Y, SCALE_X, SCALE_Y, 1, 0, 0 , 1)
+
+    for idx, collectibleSprite in ipairs(renderItems) do
+        if collectibleSprite.Sprite:IsFinished("Fade") then
+            table.remove(renderItems, idx)
+        end
+        collectibleSprite.Sprite:Render(Isaac.WorldToRenderPosition((collectibleSprite.Player).Position) + PUSH_ABOVE_ISAAC)
+        collectibleSprite.Sprite:Update()
+    end
+end
+MilkshakeVol1:AddCallback(ModCallbacks.MC_POST_RENDER, worldOfLight.PostRender)
 
 return worldOfLight
