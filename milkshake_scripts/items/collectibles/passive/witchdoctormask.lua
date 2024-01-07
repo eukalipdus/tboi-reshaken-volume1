077cba0d80757d2b554074d24c56fe02af7e0514
eucalyptusJ <88509866+eucalyptusJ@users.noreply.github.com>
2024-01-06 20:39:09 -0500

    fix: Reorder horse spirit pill hud to be accurate

--- milkshake_scripts/items/collectibles/passive/witchdoctormask.lua
+++ milkshake_scripts/items/collectibles/passive/witchdoctormask.lua
@@ -257,6 +257,7 @@ function witchDoctorMask:GetShaderParams()
                     else
                         orbPillHuds[i]:Render(position)
                         orbPillHuds[i]:SetFrame(GetFrameFromId(heldPill, pillAnimFrames) - 1)
+                        print(GetFrameFromId(heldPill, pillAnimFrames) - 1)
                         orbPillHuds[i]:Play("HUD")
                     end
                 end
