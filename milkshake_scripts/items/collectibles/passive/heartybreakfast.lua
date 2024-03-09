d4acb1fbd954f294d7092b8058b0b284b5dfd6e5
gnabble <134325525+gnabble@users.noreply.github.com>
2024-03-08 21:59:34 -0500

    hearty/balanced breakfast stat boost stacking

--- milkshake_scripts/items/collectibles/passive/heartybreakfast.lua
+++ milkshake_scripts/items/collectibles/passive/heartybreakfast.lua
@@ -6,18 +6,18 @@ local DMG_UP = 0.5
 local TEARS_UP = 0.3
 
 function heartyBreakfast:onCache(player, cacheFlag)
-    if player:HasCollectible(enums.Collectibles.HEARTY_BREAKFAST) then
-        if cacheFlag == CacheFlag.CACHE_LUCK then
-            player.Luck = player.Luck + LUCK_UP
-        end
+    local collectibleNum = player:GetCollectibleNum(enums.Collectibles.HEARTY_BREAKFAST)
 
-        if cacheFlag == CacheFlag.CACHE_DAMAGE then
-            player.Damage = player.Damage + DMG_UP
-        end
+    if cacheFlag == CacheFlag.CACHE_LUCK then
+        player.Luck = player.Luck + LUCK_UP * collectibleNum
+    end
 
-        if cacheFlag == CacheFlag.CACHE_FIREDELAY then
-            player.MaxFireDelay = utility:TearsUp(player.MaxFireDelay, TEARS_UP)
-        end
+    if cacheFlag == CacheFlag.CACHE_DAMAGE then
+        player.Damage = player.Damage + DMG_UP * collectibleNum
+    end
+
+    if cacheFlag == CacheFlag.CACHE_FIREDELAY then
+        player.MaxFireDelay = utility:TearsUp(player.MaxFireDelay, TEARS_UP * collectibleNum)
     end
 end
 MilkshakeVol1:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, heartyBreakfast.onCache)
