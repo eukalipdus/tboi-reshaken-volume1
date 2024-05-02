52bec768be80261421a36d546683bd942f7fd63e
gnabble <134325525+gnabble@users.noreply.github.com>
2024-05-02 16:29:00 -0400

    Glass Head T. Keeper fix

--- milkshake_scripts/enemies/glassheads.lua
+++ milkshake_scripts/enemies/glassheads.lua
@@ -321,7 +321,7 @@ MilkshakeVol1:AddCallback(
 MilkshakeVol1:AddCallback(ModCallbacks.MC_POST_ENTITY_KILL, function (_, entity)
     sfx:Stop(SoundEffect.SOUND_DEATH_BURST_SMALL)
 
-    entity:Remove()
+    entity.Visible = false
 
     local newEntity = TSIL.EntitySpecific.SpawnNPC(
         enums.Enemies.GLASS_HEAD,
