739bf8ca3cab740e235cb4f8d9fdbcae970ab614
thicco-catto <cristobal.martin01@gmail.com>
2024-05-21 10:50:00 +0200

    fix: Non replaceable tnt is actually non replaceable

--- milkshake_scripts/nonreplaceabletnt.lua
+++ milkshake_scripts/nonreplaceabletnt.lua
@@ -3,15 +3,24 @@ local META_ENTITY_TYPE = 618
 local META_ENTITY_VARIANT = 125
 local META_ENTITY_SUBTYPE = 0
 
-function NonReplaceableTNT:PreRoomEntitySpawn(type, variant, subtype)
+function NonReplaceableTNT:PreRoomEntitySpawn(type, variant, subtype, gridIndex)
     if type ~= META_ENTITY_TYPE
     or variant ~= META_ENTITY_VARIANT
     or subtype ~= META_ENTITY_SUBTYPE then
         return
     end
 
+    TSIL.Utils.Functions.RunNextRoom(function ()
+        TSIL.GridEntities.SpawnGridEntity(
+            GridEntityType.GRID_TNT,
+            0,
+            gridIndex,
+            true
+        )
+    end)
+
     return {
-        TSIL.Enums.GridEntityXMLType.TNT,
+        TSIL.Enums.GridEntityXMLType.DECORATION,
         0,
         0
     }
