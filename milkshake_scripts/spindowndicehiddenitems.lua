755c6c854b2b90543bce68dcbaf939e8c36440c8
eucalyptusJ <88509866+eucalyptusJ@users.noreply.github.com>
2024-09-04 22:47:45 -0400

    fix: Spindown for hidden items now also accounts for Reshaken unlocks

--- milkshake_scripts/spindowndicehiddenitems.lua
+++ milkshake_scripts/spindowndicehiddenitems.lua
@@ -1,8 +1,9 @@
 local ItemHider = {}
 local enums = MilkshakeVol1.enums
-local FINAL_VOL1_ITEM = enums.Collectibles.PRISMATIC_GOGGLES
+local utility = MilkshakeVol1.utility
 
-local brendaWisps = {
+local spindownForbiddenItems = {
+    enums.Collectibles.UNCHARGED_MIRROR_KEY,
     enums.Collectibles.SPECIAL_BRENDA_FIRE_WISP,
     enums.Collectibles.SPECIAL_BRENDA_PSYCHIC_WISP,
     enums.Collectibles.SPECIAL_BRENDA_NATURE_WISP,
@@ -13,22 +14,59 @@ local brendaWisps = {
     enums.Collectibles.SPECIAL_BRENDA_TERRA_WISP
 }
 
-function ItemHider:UseItem()
-    TSIL.Utils.Functions.RunInFramesTemporary(function ()
-        local collectibles = Isaac.FindByType(
-            EntityType.ENTITY_PICKUP,
-            PickupVariant.PICKUP_COLLECTIBLE
-        )
+local unlockableItems = {
+    enums.Collectibles.PRISMATIC_GOGGLES
+}
 
-        for _, currentCollectible in pairs(collectibles) do
-            if TSIL.Utils.Tables.IsIn(brendaWisps, currentCollectible) then
-                currentCollectible:Morph(
-                    currentCollectible.Type,
-                    currentCollectible.Variant,
-                    FINAL_VOL1_ITEM
-                )
-            end
-        end
-    end, 1)
+function ItemHider:UseItem()
+    local preSpindowncollectibles = Isaac.FindByType(
+        EntityType.ENTITY_PICKUP,
+        PickupVariant.PICKUP_COLLECTIBLE
+    )
+
+    for _, currentCollectible in pairs(preSpindowncollectibles) do
+        utility:SetData(
+            currentCollectible,
+            "WillSpindown",
+            true
+        )
+    end
 end
-MilkshakeVol1:AddCallback(ModCallbacks.MC_USE_ITEM, ItemHider.UseItem, CollectibleType.COLLECTIBLE_SPINDOWN_DICE)
+MilkshakeVol1:AddCallback(ModCallbacks.MC_PRE_USE_ITEM, ItemHider.UseItem, CollectibleType.COLLECTIBLE_SPINDOWN_DICE)
+
+function ItemHider:PostPickupInit(pickup)
+    if not utility:GetData(pickup, "WillSpindown") then
+        return
+    end
+
+    utility:SetData(pickup, "WillSpindown", false)
+
+    local isForbidden = TSIL.Utils.Tables.IsIn(spindownForbiddenItems, pickup.SubType)
+    local isUnlockable = TSIL.Utils.Tables.IsIn(unlockableItems, pickup.SubType)
+
+    if not pickup.Variant == PickupVariant.PICKUP_COLLECTIBLE
+    or not (isForbidden or isUnlockable) then
+        return
+    end
+
+    local newCollectibleType = pickup.SubType - 1
+
+    if isUnlockable then
+        local achievementToCheck = MilkshakeVol1.UnlockManager:GetCollectibleAssociatedAchievement(pickup.SubType)
+
+        if not achievementToCheck then
+            return
+        end
+
+        if not MilkshakeVol1.UnlockManager:IsAchievementUnlocked(achievementToCheck) then
+            newCollectibleType = newCollectibleType - 1
+        end
+    end
+
+    pickup:Morph(
+        EntityType.ENTITY_PICKUP,
+        PickupVariant.PICKUP_COLLECTIBLE,
+        newCollectibleType
+    )
+end
+MilkshakeVol1:AddCallback(ModCallbacks.MC_POST_PICKUP_INIT, ItemHider.PostPickupInit)
