c62494d3fbef6676175041910172038e63c6bd21
Kattack <29678669+Kattacka@users.noreply.github.com>
2024-09-15 17:52:45 -0400

    Tweaked and Fixed Chromatic Prism
    Improved enemy diffraction effect visual.
    Fixed luck chance being inverted.
    Changed chance to apply to enemy to be 25% + 2.5% per luck (same as athame)
    Made base chance increase by 25% per extra item copy.
    Allowed it to apply to bosses
    Fixed it not applying to any enemies at all if it was supposed to apply to 5 or more enemies.

--- milkshake_scripts/items/collectibles/passive/prismatic_goggles.lua
+++ milkshake_scripts/items/collectibles/passive/prismatic_goggles.lua
@@ -5,19 +5,19 @@ local utility = MilkshakeVol1.utility
 
 local PRISMATIC_GOGGLES = MilkshakeVol1.enums.Collectibles.PRISMATIC_GOGGLES
 
-local BASE_DIFFRACTION_CHANCE = 0.15
-local DIFFRACTION_LUCK_INCREASE = 0.05
+local BASE_DIFFRACTION_CHANCE = 0.25
+local DIFFRACTION_LUCK_INCREASE = 0.025
 local DIFFRACTION_CAP = 5
 
 local LASER_DAMAGE = 8
 local LASER_DAMAGE_SCALING = 2
 local LASER_ONE_HIT = true
 
-local REFLECTION_ALPHA = 0.3
-local REFLECTION_BRIGHTNESS = 0.2
+local REFLECTION_ALPHA = 0.2
+local REFLECTION_BRIGHTNESS = 0.5
 
-local REFLECTION_SWING_SPEED = 0.1
-local REFLECTION_AMPLITUDE = 15
+local REFLECTION_SWING_SPEED = 0.05
+local REFLECTION_AMPLITUDE = 5
 
 local ROTATION_SPEED = 1
 
@@ -46,9 +46,9 @@ function MilkshakeVol1.API:IsEnemyDiffracted(target)
 end
 
 local TINTS = {
-    {1,0,0},
-    {0,1,0},
-    {0,0,1},
+    {1,0,0, REFLECTION_BRIGHTNESS,0,0},
+    {0,1,0, 0,REFLECTION_BRIGHTNESS,0},
+    {0,0,1, 0,0,REFLECTION_BRIGHTNESS},
 }
 
 ---@param color Color
@@ -75,21 +75,22 @@ local function ApplyDiffraction(player)
     local rng = player:GetCollectibleRNG(PRISMATIC_GOGGLES)
     local cap = player:GetCollectibleNum(PRISMATIC_GOGGLES) * DIFFRACTION_CAP
     local currentCap = 0
-    local chanceMultiplier = math.max(1, player.Luck + player:GetCollectibleNum(PRISMATIC_GOGGLES))
-    local diffractionChance = BASE_DIFFRACTION_CHANCE + DIFFRACTION_LUCK_INCREASE * chanceMultiplier
+
+    local diffractionChance = (BASE_DIFFRACTION_CHANCE*player:GetCollectibleNum(PRISMATIC_GOGGLES)) + (DIFFRACTION_LUCK_INCREASE * player.Luck)
+    print(diffractionChance)
     for _,entity in ipairs(Isaac.GetRoomEntities()) do
         if not entity:IsVulnerableEnemy()
-        or entity:IsBoss()
+       -- or entity:IsBoss() Replace with max health + is boss check (return if is boss and health >200)
         or utility:GetData(entity, "DiffractionSource")
-        or rng:RandomFloat() < diffractionChance
+        or rng:RandomFloat() > diffractionChance
         then
             goto continue
         end
         currentCap = currentCap + 1
+        utility:SetData(entity, "DiffractionSource", player)
         if currentCap > cap then
             return
         end
-        utility:SetData(entity, "DiffractionSource", player)
         ::continue::
     end
 end
@@ -105,7 +106,7 @@ function prismaticGoggles:DiffractionRender(npc)
     local offsetVector = OffsetVector(npc)
     for index, tint in ipairs(TINTS) do
         -- I REALLY REALLY wanted to use table.unpack here, but for some reason it wasn't working correctly.
-        sprite.Color = Color(tint[1], tint[2], tint[3], REFLECTION_ALPHA, REFLECTION_BRIGHTNESS, REFLECTION_BRIGHTNESS, REFLECTION_BRIGHTNESS)
+        sprite.Color = Color(tint[1], tint[2], tint[3], REFLECTION_ALPHA, tint[4], tint[5], tint[6])
         local drawPos = Isaac.WorldToScreen(npc.Position + offsetVector:Rotated(120*index))
         sprite:Render(drawPos)
     end
