4d58d3f9e5d32393abebc5f216d5192dffc2a0b6
Eucalyptus <eucalyptusj109@gmail.com>
2024-09-29 17:14:52 -0400

    fix: Many enemy orbs being set to nil instead of Chaos Orb (enum is RANDOM, not CHAOS)

--- milkshake_scripts/modcompatibility/retribution.lua
+++ milkshake_scripts/modcompatibility/retribution.lua
@@ -7,7 +7,7 @@ MilkshakeVol1:AddModCompatibility("Retribution", function()
         { orb = MilkshakeVol1.enums.Orbs.FIRE,   type = 793, variant = 1892 },    -- pig bang
         { orb = MilkshakeVol1.enums.Orbs.HOLY,   type = 950, variant = 1873 },    -- hogma
         { orb = MilkshakeVol1.enums.Orbs.UNHOLY, type = 38,  variant = 1,    subtype = 118 }, -- samael angel
-        { orb = MilkshakeVol1.enums.Orbs.CHAOS,  type = 15,  variant = 1873 },    -- curdle
+        { orb = MilkshakeVol1.enums.Orbs.RANDOM,  type = 15,  variant = 1873 },    -- curdle
         { orb = MilkshakeVol1.enums.Orbs.POISON, type = 282, variant = 1873 },    -- mega clot
         { orb = MilkshakeVol1.enums.Orbs.FIRE,   type = 25,  variant = 1873 },    -- kaboom fly
         { orb = MilkshakeVol1.enums.Orbs.WATER,  type = 25,  variant = 1874 },    -- bloated fly
@@ -24,13 +24,13 @@ MilkshakeVol1:AddModCompatibility("Retribution", function()
         { orb = MilkshakeVol1.enums.Orbs.NATURE, type = 61,  variant = 1873 },    -- full sucker
         { orb = MilkshakeVol1.enums.Orbs.POISON, type = 61,  variant = 1874 },    -- full spit
         { orb = MilkshakeVol1.enums.Orbs.WATER,  type = 61,  variant = 1875 },    -- sip
-        { orb = MilkshakeVol1.enums.Orbs.CHAOS,  type = 88,  variant = 0,    subtype = 183 }, -- stumbling boil
+        { orb = MilkshakeVol1.enums.Orbs.RANDOM,  type = 88,  variant = 0,    subtype = 183 }, -- stumbling boil
         { orb = MilkshakeVol1.enums.Orbs.POISON, type = 88,  variant = 1,    subtype = 183 }, -- stumbling gut
-        { orb = MilkshakeVol1.enums.Orbs.CHAOS,  type = 88,  variant = 2,    subtype = 183 }, -- stumbling sack
-        { orb = MilkshakeVol1.enums.Orbs.CHAOS,  type = 240, variant = 1873 },    -- wall writhe
-        { orb = MilkshakeVol1.enums.Orbs.CHAOS,  type = 242, variant = 1873 },    -- blind writhe
+        { orb = MilkshakeVol1.enums.Orbs.RANDOM,  type = 88,  variant = 2,    subtype = 183 }, -- stumbling sack
+        { orb = MilkshakeVol1.enums.Orbs.RANDOM,  type = 240, variant = 1873 },    -- wall writhe
+        { orb = MilkshakeVol1.enums.Orbs.RANDOM,  type = 242, variant = 1873 },    -- blind writhe
         { orb = MilkshakeVol1.enums.Orbs.UNHOLY, type = 241, variant = 1873 },    -- rage writhe
-        { orb = MilkshakeVol1.enums.Orbs.CHAOS,  type = 288, variant = 1873 },    -- huskie
+        { orb = MilkshakeVol1.enums.Orbs.RANDOM,  type = 288, variant = 1873 },    -- huskie
         { orb = MilkshakeVol1.enums.Orbs.NATURE, type = 244, variant = 1873 },    -- pinhead
         { orb = MilkshakeVol1.enums.Orbs.FIRE,   type = 255, variant = 1873 },    -- pinprick
         { orb = MilkshakeVol1.enums.Orbs.UNDEAD, type = 88,  variant = 0,    subtype = 184 }, -- walking blue boil
--- milkshake_scripts/modcompatibility/revelations.lua
+++ milkshake_scripts/modcompatibility/revelations.lua
@@ -17,10 +17,10 @@ MilkshakeVol1:AddModCompatibility("REVEL", function()
         { orb = MilkshakeVol1.enums.Orbs.WATER,   type = 462, variant = 541 },   -- geicer
         { orb = MilkshakeVol1.enums.Orbs.WATER,   type = 455, variant = 541 },   -- iced spider
         { orb = MilkshakeVol1.enums.Orbs.POISON,  type = 455, variant = 542 },   -- yellow iced spider
-        { orb = MilkshakeVol1.enums.Orbs.CHAOS,   type = 476, variant = 541 },   -- drifty
+        { orb = MilkshakeVol1.enums.Orbs.RANDOM,   type = 476, variant = 541 },   -- drifty
         { orb = MilkshakeVol1.enums.Orbs.WATER,   type = 22,  variant = 541 },   -- hice
         { orb = MilkshakeVol1.enums.Orbs.WATER,   type = 478, variant = 541 },   -- cloudy
-        { orb = MilkshakeVol1.enums.Orbs.CHAOS,   type = 480, variant = 541 },   -- smolycephalus
+        { orb = MilkshakeVol1.enums.Orbs.RANDOM,   type = 480, variant = 541 },   -- smolycephalus
         { orb = MilkshakeVol1.enums.Orbs.WATER,   type = 481, variant = 338 },   -- ice hazard gaper
         { orb = MilkshakeVol1.enums.Orbs.WATER,   type = 481, variant = 337 },   -- ice hazard horf
         { orb = MilkshakeVol1.enums.Orbs.WATER,   type = 481, variant = 336 },   -- ice hazard hopper
@@ -30,7 +30,7 @@ MilkshakeVol1:AddModCompatibility("REVEL", function()
         { orb = MilkshakeVol1.enums.Orbs.FIRE,    type = 481, variant = 332 },   -- ice hazard troll bomb
         { orb = MilkshakeVol1.enums.Orbs.WATER,   type = 481, variant = 331 },   -- ice hazard i.blob
         { orb = MilkshakeVol1.enums.Orbs.WATER,   type = 481, variant = 330 },   -- ice hazard empty
-        { orb = MilkshakeVol1.enums.Orbs.CHAOS,   type = 476, variant = 542 },   -- pucker
+        { orb = MilkshakeVol1.enums.Orbs.RANDOM,   type = 476, variant = 542 },   -- pucker
         { orb = MilkshakeVol1.enums.Orbs.WATER,   type = 483, variant = 541 },   -- brainfreeze
         { orb = MilkshakeVol1.enums.Orbs.WATER,   type = 485, variant = 541 },   -- rolling snowball
         { orb = MilkshakeVol1.enums.Orbs.WATER,   type = 485, variant = 542 },   -- snot rocket
@@ -43,7 +43,7 @@ MilkshakeVol1:AddModCompatibility("REVEL", function()
         { orb = MilkshakeVol1.enums.Orbs.WATER,   type = 487, variant = 335, subtype = 1 }, -- ice wraith body
         { orb = MilkshakeVol1.enums.Orbs.FIRE,    type = 487, variant = 333 },   -- grill o' wisp
         { orb = MilkshakeVol1.enums.Orbs.FIRE,    type = 487, variant = 333, subtype = 1 }, -- small grill o' wisp
-        { orb = MilkshakeVol1.enums.Orbs.CHAOS,   type = 488, variant = 541 },   -- brother bloody
+        { orb = MilkshakeVol1.enums.Orbs.RANDOM,   type = 488, variant = 541 },   -- brother bloody
         { orb = MilkshakeVol1.enums.Orbs.NATURE,  type = 634, variant = 541 },   -- ice pooter
         { orb = MilkshakeVol1.enums.Orbs.WATER,   type = 489, variant = 541 },   -- iced hive
         { orb = MilkshakeVol1.enums.Orbs.WATER,   type = 490, variant = 541 },   -- stalactrite
@@ -67,7 +67,7 @@ MilkshakeVol1:AddModCompatibility("REVEL", function()
         { orb = MilkshakeVol1.enums.Orbs.PSYCHIC, type = 742, variant = 163 },   -- anima
         { orb = MilkshakeVol1.enums.Orbs.NATURE,  type = 743, variant = 541 },   -- locust
         { orb = MilkshakeVol1.enums.Orbs.UNDEAD,  type = 744, variant = 541 },   -- rag bony
-        { orb = MilkshakeVol1.enums.Orbs.CHAOS,   type = 745, variant = 541 },   -- mother pucker
+        { orb = MilkshakeVol1.enums.Orbs.RANDOM,   type = 745, variant = 541 },   -- mother pucker
         { orb = MilkshakeVol1.enums.Orbs.NATURE,  type = 746, variant = 541 },   -- aerotoma
         { orb = MilkshakeVol1.enums.Orbs.UNDEAD,  type = 747, variant = 541, subtype = 0 }, -- rag trite
         { orb = MilkshakeVol1.enums.Orbs.POISON,  type = 748, variant = 541 },   -- innard
@@ -92,7 +92,7 @@ MilkshakeVol1:AddModCompatibility("REVEL", function()
         { orb = MilkshakeVol1.enums.Orbs.NATURE,  type = 763, variant = 541 },   -- peashy
         { orb = MilkshakeVol1.enums.Orbs.NATURE,  type = 763, variant = 542 },   -- peashy nail
         { orb = MilkshakeVol1.enums.Orbs.NATURE,  type = 763, variant = 542, subtype = 1 }, -- peashy nail (in ground)
-        { orb = MilkshakeVol1.enums.Orbs.CHAOS,   type = 764, variant = 541, subtype = 0 }, -- chicken
+        { orb = MilkshakeVol1.enums.Orbs.RANDOM,   type = 764, variant = 541, subtype = 0 }, -- chicken
         { orb = MilkshakeVol1.enums.Orbs.ROCK,    type = 240, variant = 541 },   -- stone creep
         { orb = MilkshakeVol1.enums.Orbs.ROCK,    type = 218, variant = 541 },   -- button masher
         { orb = MilkshakeVol1.enums.Orbs.ROCK,    type = 766, variant = 541 },   -- slambip
@@ -102,8 +102,8 @@ MilkshakeVol1:AddModCompatibility("REVEL", function()
         { orb = MilkshakeVol1.enums.Orbs.NATURE,  type = 524, variant = 541 },   -- antlion baby
         { orb = MilkshakeVol1.enums.Orbs.NATURE,  type = 526, variant = 541 },   -- antlion egg
         { orb = MilkshakeVol1.enums.Orbs.NATURE,  type = 3,   variant = 1778 },  -- antlion familiar
-        { orb = MilkshakeVol1.enums.Orbs.CHAOS,   type = 925, variant = 543 },   -- skitterpill card
-        { orb = MilkshakeVol1.enums.Orbs.CHAOS,   type = 449, variant = 541 },   -- smolycephalus for scale
+        { orb = MilkshakeVol1.enums.Orbs.RANDOM,   type = 925, variant = 543 },   -- skitterpill card
+        { orb = MilkshakeVol1.enums.Orbs.RANDOM,   type = 449, variant = 541 },   -- smolycephalus for scale
         { orb = MilkshakeVol1.enums.Orbs.ROCK,    type = 203, variant = 541 },   -- big blowy
         { orb = MilkshakeVol1.enums.Orbs.WATER,   type = 203, variant = 542 },   -- igloo
         { orb = MilkshakeVol1.enums.Orbs.ROCK,    type = 203, variant = 543 },   -- frost shooter
