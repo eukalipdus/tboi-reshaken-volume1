9e393467ef19af5e49e7d5ecb37fda22be8cc2a2
Eucalyptus <eucalyptusj109@gmail.com>
2024-10-14 23:40:16 -0400

    Brenda now gives the custom unholy/undead wisps

--- milkshake_scripts/slots/spiritklinbrenda.lua
+++ milkshake_scripts/slots/spiritklinbrenda.lua
@@ -94,8 +94,8 @@ local possibleWisps = {
     enums.Collectibles.SPECIAL_BRENDA_WATER_WISP,
     enums.Collectibles.SPECIAL_BRENDA_POISON_WISP,
     enums.Collectibles.SPECIAL_BRENDA_HOLY_WISP,
-    CollectibleType.COLLECTIBLE_BOOK_OF_THE_DEAD,
-    CollectibleType.COLLECTIBLE_SATANIC_BIBLE,
+    enums.Collectibles.SPECIAL_BRENDA_UNHOLY_WISP,
+    enums.Collectibles.SPECIAL_BRENDA_DEAD_WISP,
     enums.Collectibles.SPECIAL_BRENDA_TERRA_WISP,
 }
 
