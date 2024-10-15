b4ffa93d30d82148c6aa98e74e1ed371a1c5a625
Kattack <29678669+Kattacka@users.noreply.github.com>
2024-10-15 00:38:14 -0400

    Tweak chromatic prism damage

--- milkshake_scripts/items/collectibles/passive/prismatic_goggles.lua
+++ milkshake_scripts/items/collectibles/passive/prismatic_goggles.lua
@@ -10,7 +10,7 @@ local DIFFRACTION_LUCK_INCREASE = 0.025
 
 local MAX_DIFFRACTED_BOSS_HEALTH = 200
 
-local LASER_DAMAGE = 1
+local LASER_DAMAGE = 0
 local LASER_DAMAGE_SCALING = 1
 local LASER_DURATION = 8
 local LASER_ONE_HIT = false
@@ -139,7 +139,7 @@ function prismaticGoggles:LasersOnDeath(npc)
     ---@cast source EntityPlayer
     if not source then return end
 
-    local damage = (LASER_DAMAGE + LASER_DAMAGE_SCALING * game:GetLevel():GetAbsoluteStage()) + ((source.Damage)/2)
+    local damage = (LASER_DAMAGE + LASER_DAMAGE_SCALING * MilkshakeVol1.utility:GetCurrentChapter()) + ((source.Damage))
     local offsetVector = Vector(math.random(-1, 1) + math.random(), math.random(-1, 1) + math.random())
     local initialAngle = 0
     for index, tint in ipairs(TINTS) do
