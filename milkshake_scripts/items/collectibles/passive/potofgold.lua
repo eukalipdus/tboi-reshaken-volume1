b0bf21562375f1143894096211bd8adec276aa86
eucalyptusJ <88509866+eucalyptusJ@users.noreply.github.com>
2024-10-30 00:25:41 -0400

    Fix Acid and Crystal Pennies not being removed due to Isaac animating

--- milkshake_scripts/items/collectibles/passive/potofgold.lua
+++ milkshake_scripts/items/collectibles/passive/potofgold.lua
@@ -215,12 +215,12 @@ function potOfGold:PrePickupCollision(pickup, collider)
 
         if not rainbowPenny then return end
 
-        rainbowPenny.onPickup(pickup, player)
-
         pickup:Die()
-
         MilkshakeVol1.utility:SetData(pickup, "IsRainbowPenny", true)
         pickup.SubType = CoinSubType.COIN_PENNY
+
+        -- NOTE: This should be changed to RunInFramesTemporary once the TSIL bug involving it is fixed
+        TSIL.Utils.Functions.RunInFrames(rainbowPenny.onPickup, 1, pickup, player)
     end
 end
 MilkshakeVol1:AddCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, potOfGold.PrePickupCollision)
