cde0a372fff5aaab3158cf2547a7a7f907627918
dan2 <hazrd6@gmail.com>
2024-11-15 09:58:42 +0500

    fire orb new room fix 3

--- milkshake_scripts/items/pick ups/fireorb.lua
+++ milkshake_scripts/items/pick ups/fireorb.lua
@@ -256,14 +256,6 @@ function CheckExhaling(player)
 	local info = GetExhalingInfo(player)
 	if not info then return end
 
-	-- new room fire orb cancel
-	if Game():GetRoom():GetFrameCount() == 0 then
-		RemoveExhalingInfo(player)
-		player:TryRemoveNullCostume(enums.Costumes.INFERNO_ORB)
-		utility:SetBlindfold(player, false)
-		return
-	end
-
 	info.timer = info.timer - 1
 	if info.timer > 0 then return end
 
@@ -299,6 +291,18 @@ end
 
 ---@param player EntityPlayer
 function RubyOrb:PostPEffectUpdate(player)
+
+-- 	local info = GetExhalingInfo(player)
+-- 	if not info then return end
+--
+-- 	-- new room fire orb cancel
+-- 	if Game():GetRoom():GetFrameCount() == 0 then
+-- 		RemoveExhalingInfo(player)
+-- 		player:TryRemoveNullCostume(enums.Costumes.INFERNO_ORB)
+-- 		utility:SetBlindfold(player, false)
+-- 		return
+-- 	end
+
 	CheckInhaling(player)
 
 	CheckExhaling(player)
@@ -434,10 +438,11 @@ MilkshakeVol1:AddCallback(
 	EntityType.ENTITY_TEAR
 )
 
---[[
+--[
 function RubyOrb:OnNewRoomEarly()
 	for _, player in ipairs(TSIL.Players.GetPlayers()) do
-		if GetExhalingInfo(player) then
+		if GetInhalingInfo(player) or GetExhalingInfo(player) then
+			RemoveInhalingInfo(player)
 			RemoveExhalingInfo(player)
 			player:TryRemoveNullCostume(enums.Costumes.INFERNO_ORB)
 			utility:SetBlindfold(player, false)
@@ -449,4 +454,4 @@ MilkshakeVol1:AddPriorityCallback(
 	math.mininteger,
 	RubyOrb.OnNewRoomEarly
 )
---]]
+--]
