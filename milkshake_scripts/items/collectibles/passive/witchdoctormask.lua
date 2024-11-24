cfa7cdd52c76607fa7cf0d30cda86367a3f13076
Eucalyptus <eucalyptusj109@gmail.com>
2024-11-24 15:35:32 -0500

    Fix Witch Doctor Mask not spawning pills if Tainted Isaac picked it up first

--- milkshake_scripts/items/collectibles/passive/witchdoctormask.lua
+++ milkshake_scripts/items/collectibles/passive/witchdoctormask.lua
@@ -288,8 +288,10 @@ MilkshakeVol1:AddCallback(ModCallbacks.MC_GET_SHADER_PARAMS, witchDoctorMask.Get
 
 function witchDoctorMask:PostPlayerCollectibleAdded(player, collectible, firstTime)
     if collectible ~= enums.Collectibles.WITCH_DOCTOR_MASK
-    or ((firstTime == false) and #(TSIL.Players.GetPlayersOfType(PlayerType.PLAYER_ISAAC_B)) > 0)
-    or player.Variant == 1 then return end
+    or (player:GetPlayerType() == PlayerType.PLAYER_ISAAC_B and firstTime == false)
+    or player.Variant == 1 then
+        return
+    end
     local roll = TSIL.Random.GetRandomInt(1, PillColor.NUM_PILLS)
     local spawnPos = Isaac.GetFreeNearPosition(player.Position, SPAWN_DISTANCE)
     TSIL.PickupSpecific.SpawnPill(roll, spawnPos)
