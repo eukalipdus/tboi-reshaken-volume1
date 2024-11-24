7db82fe20954c89255638eb8dc0e8053bc9f33e2
Eucalyptus <eucalyptusj109@gmail.com>
2024-11-24 14:46:07 -0500

    Fix Mirror Key spawning shop pickups when entering a shop mirror room

--- milkshake_scripts/items/collectibles/active/mirrorkey.lua
+++ milkshake_scripts/items/collectibles/active/mirrorkey.lua
@@ -591,7 +591,11 @@ function MirrorKey:OnNewRoom()
     AddLostCurse()
     UpdateDamageBonusCache()
     RemoveTallLadder()
-    RespawnSavedPickups()
+
+    if prevRoomIndex == level:GetCurrentRoomIndex() then
+        RespawnSavedPickups()
+    end
+
     SavePickupData()
 
     if EID then
@@ -1063,4 +1067,18 @@ MilkshakeVol1:AddCallback(
     ModCallbacks.MC_EVALUATE_CACHE,
     MirrorKey.EvaluateCache,
     CacheFlag.CACHE_DAMAGE
+)
+
+function MirrorKey:PostGameStarted()
+    local roomIndex = Game():GetLevel():GetCurrentRoomIndex()
+
+    TSIL.SaveManager.SetPersistentVariable(
+        MilkshakeVol1,
+        "PreviousRoomIndex",
+        roomIndex
+    )
+end
+MilkshakeVol1:AddCallback(
+    ModCallbacks.MC_POST_GAME_STARTED,
+    MirrorKey.PostGameStarted
 )
