5c4d4e2d2deac26def563e0417d9342f5623059b
Kattack <29678669+Kattacka@users.noreply.github.com>
2026-06-07 17:47:54 -0400

    Fixed spirit orbs spawning duplicates if you leave an active room and come back

--- milkshake_scripts/items/pick ups/cardspawner.lua
+++ milkshake_scripts/items/pick ups/cardspawner.lua
@@ -10,15 +10,25 @@ local CARD_SPAWN_VARIANT = 124
 function CardSpawner:PreEntitySpawn(type, variant, subtype, _, _, _, seed)
     if type ~= CARD_SPAWN_TYPE then return end
     if variant ~= CARD_SPAWN_VARIANT then return end
-    local card = MilkshakeVol1.enums.CardSpawnerSubtypePerCard[subtype]
-    if not card then return end
 
-    return {
-        EntityType.ENTITY_PICKUP,
-        PickupVariant.PICKUP_TAROTCARD,
-        card,
-        seed
-    }
+    if Game():GetRoom():IsFirstVisit() then
+        local card = MilkshakeVol1.enums.CardSpawnerSubtypePerCard[subtype]
+        if not card then return end
+
+        return {
+            EntityType.ENTITY_PICKUP,
+            PickupVariant.PICKUP_TAROTCARD,
+            card,
+            seed
+        }
+    else
+        return {
+            1000,
+            40,
+            0,
+            seed
+        }
+    end
 end
 MilkshakeVol1:AddCallback(
     ModCallbacks.MC_PRE_ENTITY_SPAWN,
