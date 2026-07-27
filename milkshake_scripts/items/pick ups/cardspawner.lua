44da2bf356f3aadfc13c1eef63afc286b2f1328a
Kattack <29678669+Kattacka@users.noreply.github.com>
2026-07-27 04:23:06 -0400

    fixed spirit orb pickups spawning in the mirror world

--- milkshake_scripts/items/pick ups/cardspawner.lua
+++ milkshake_scripts/items/pick ups/cardspawner.lua
@@ -10,8 +10,9 @@ local CARD_SPAWN_VARIANT = 124
 function CardSpawner:PreEntitySpawn(type, variant, subtype, _, _, _, seed)
     if type ~= CARD_SPAWN_TYPE then return end
     if variant ~= CARD_SPAWN_VARIANT then return end
+    local room = Game():GetRoom()
 
-    if Game():GetRoom():IsFirstVisit() then
+    if room:IsFirstVisit() and (not room:IsMirrorWorld()) and MilkshakeVol1.enums.CardSpawnerSubtypePerCard[subtype] then
         local card = MilkshakeVol1.enums.CardSpawnerSubtypePerCard[subtype]
         if not card then return end
 
